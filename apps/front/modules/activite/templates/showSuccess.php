c8d88bb37a8195043759f5c2d91f0c62e79c42f3
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-05-21 15:48:41 +0000

    New feature : we can see detailed transaction list for each activity in 'Activite' module.

--- apps/front/modules/activite/templates/showSuccess.php
+++ apps/front/modules/activite/templates/showSuccess.php
@@ -1,30 +1,61 @@
-<table>
+<?php use_helper('Date') ?>
+<?php use_helper('Membre') ?>
+<?php use_helper('Number') ?>
+
+<h2>Détails pour "<?php echo $activite->getLibelle() ?>"</h2>
+
+<table class="tableauDetails">
   <tbody>
     <tr>
-      <th>Id:</th>
-      <td><?php echo $activite->getId() ?></td>
+      <th><?php echo image_tag('time.png', 'align="absmiddle"')?> Enregistrée le :</th>
+      <td><?php echo format_datetime($activite->getCreatedAt(), 'dd/MM/yyyy HH:mm') ?> par <?php echo format_membre($activite->getEnregistrePar()) ?></td>
     </tr>
     <tr>
-      <th>Libelle:</th>
-      <td><?php echo $activite->getLibelle() ?></td>
-    </tr>
-    <tr>
-      <th>Actif:</th>
-      <td><?php echo $activite->getActif() ?></td>
-    </tr>
-    <tr>
-      <th>Created at:</th>
-      <td><?php echo $activite->getCreatedAt() ?></td>
-    </tr>
-    <tr>
-      <th>Updated at:</th>
-      <td><?php echo $activite->getUpdatedAt() ?></td>
+      <th><?php echo image_tag('time.png', 'align="absmiddle"')?> Mise à jour le :</th>
+      <td><?php echo format_datetime($activite->getUpdatedAt(), 'dd/MM/yyyy HH:mm') ?> par <?php echo format_membre($activite->getMisAJourPar())?></td>
     </tr>
   </tbody>
 </table>
 
-<hr />
 
-<a href="<?php echo url_for('activite/edit?id='.$activite->getId()) ?>">Edit</a>
-&nbsp;
-<a href="<?php echo url_for('activite/index') ?>">List</a>
+<h3>Recettes et dépenses</h3>
+
+<?php
+$totalRecettes = 0;
+$totalDepenses = 0;
+?>
+
+<table class="tableauDonnees">
+	<thead>
+		<tr class="enteteTableauDonnees">
+			<th>Libellé</th>
+			<th>Débit</th>
+			<th>Crédit</th>
+			<th>Date</th>
+		</tr>
+	</thead>
+
+	<?php foreach ($data as $entry): ?>
+	<tr>
+		<td><?php echo $entry->getLibelle() ?></td>
+		<?php if ($entry->getRawValue() instanceof Depense):?>
+			<td class="compteNegatif"><?php echo format_currency($entry->getMontant()); $totalDepenses += $entry->getMontant() ?></td>
+			<td>&nbsp;</td>
+		<?php else: ?>
+			<td>&nbsp;</td>
+			<td class="comptePositif"><?php echo format_currency($entry->getMontant()); $totalRecettes += $entry->getMontant() ?></td>
+		<?php endif; ?>
+		<td><?php echo format_date($entry->getDate()) ?></td>
+	</tr>
+	<?php endforeach; ?>
+
+	<tr>
+		<td colspan="4">&nbsp;</td>
+	</tr>
+	<tr class="<?php echo ($totalRecettes - $totalDepenses < 0) ? 'compteNegatif' : 'comptePositif'; ?>">
+		<td><strong>Total</strong></td>
+		<td><?php echo format_currency($totalDepenses); ?></td>
+		<td><?php echo format_currency($totalRecettes); ?></td>
+		<td><?php echo format_currency($totalRecettes - $totalDepenses) ?></td>
+	</tr>
+</table>
