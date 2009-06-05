5ea23ae2f9e17a9cd3112beca6ee5d0b62bfca2f
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-06-05 13:39:30 +0000

    Little improvement in default ACL template.

--- apps/front/modules/membre/templates/aclSuccess.php
+++ apps/front/modules/membre/templates/aclSuccess.php
@@ -1,4 +1,7 @@
 <h2>Gestion des droits</h2>
-<?php
-
-?>
+<p>
+    Cette page servira uniquement à faciliter l'ajout de droits directement
+    après la création d'un nouvel utilisateur.<br />
+    Pour éditer les droits d'un utilisateur existant, veuillez pour le moment
+    vous rendre dans la page d'édition d'un profil utilisateur.
+</p>
