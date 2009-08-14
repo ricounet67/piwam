d2ef2b09fa19cf2c33d94006892a2e259e595ce9
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-08-14 14:07:36 +0000

    Error in constraint name 'acl_action_FK_1' has been fixed.

--- trunk/data/updates/1.1.0beta-to-1.1/r92.sql
+++ trunk/data/updates/1.1.0beta-to-1.1/r92.sql
@@ -2,7 +2,7 @@
 -- Rajoute des suppressions en cascade pour faciliter la gestion
 --
 
-ALTER TABLE  `acl_action` DROP FOREIGN KEY  `acl_action_ibfk_1` ;
+ALTER TABLE  `acl_action` DROP FOREIGN KEY  `acl_action_FK_1` ;
 
 ALTER TABLE  `acl_action` ADD FOREIGN KEY (  `acl_module_id` ) REFERENCES `acl_module` (
 `id`
