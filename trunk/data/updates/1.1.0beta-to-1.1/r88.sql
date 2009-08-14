62777a5df652550a45a83ed9966c0b6854bb46d7
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-08-14 13:59:31 +0000

    Explicit DB names have been removed in SQL files.

--- trunk/data/updates/1.1.0beta-to-1.1/r88.sql
+++ trunk/data/updates/1.1.0beta-to-1.1/r88.sql
@@ -4,12 +4,12 @@
 
 ALTER TABLE  `acl_credential` DROP FOREIGN KEY  `acl_credential_FK_1` ;
 
-ALTER TABLE  `acl_credential` ADD FOREIGN KEY (  `membre_id` ) REFERENCES  `piwam-test`.`membre` (
+ALTER TABLE  `acl_credential` ADD FOREIGN KEY (  `membre_id` ) REFERENCES  `membre` (
 `id`
 ) ON DELETE CASCADE ON UPDATE CASCADE ;
 
 ALTER TABLE  `acl_credential` DROP FOREIGN KEY  `acl_credential_FK_2` ;
 
-ALTER TABLE  `acl_credential` ADD FOREIGN KEY (  `acl_action_id` ) REFERENCES  `piwam-test`.`acl_action` (
+ALTER TABLE  `acl_credential` ADD FOREIGN KEY (  `acl_action_id` ) REFERENCES `acl_action` (
 `id`
 ) ON DELETE CASCADE ON UPDATE CASCADE ;
--- trunk/data/updates/1.1.0beta-to-1.1/r92.sql
+++ trunk/data/updates/1.1.0beta-to-1.1/r92.sql
@@ -4,12 +4,12 @@
 
 ALTER TABLE  `acl_action` DROP FOREIGN KEY  `acl_action_ibfk_1` ;
 
-ALTER TABLE  `acl_action` ADD FOREIGN KEY (  `acl_module_id` ) REFERENCES  `piwam-test`.`acl_module` (
+ALTER TABLE  `acl_action` ADD FOREIGN KEY (  `acl_module_id` ) REFERENCES `acl_module` (
 `id`
 ) ON DELETE CASCADE ON UPDATE CASCADE ;
 
 ALTER TABLE  `config_variable` DROP FOREIGN KEY  `config_variable_FK_1` ;
 
-ALTER TABLE  `config_variable` ADD FOREIGN KEY (  `categorie_code` ) REFERENCES  `piwam-test`.`config_categorie` (
+ALTER TABLE  `config_variable` ADD FOREIGN KEY (  `categorie_code` ) REFERENCES `config_categorie` (
 `code`
 ) ON DELETE CASCADE ON UPDATE CASCADE ;
