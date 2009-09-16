76626f885beb2de40df282abd630f16dd77ffe3b
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-09-16 16:27:32 +0000

    User friendly error messages when cache or log are not writable.

--- apps/front/config/error/error.html.php
+++ apps/front/config/error/error.html.php
@@ -1,26 +1,83 @@
+<?php
+/*
+ * Get the file which is not writable, according to a message produced by
+ * a sfFileException
+ */
+function getUnwritableFolder($message)
+{
+    $a      = explode("\"", $message);
+    $file   = $a[1];
+    $a      = explode("/", $file);
+    $folder = $a[count($a) - 2];
+
+    return $folder;
+}
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="language" content="fr" />
-    <title>Piwam : erreur</title>
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <style type="text/css">
-        body {
-            padding: 10px;
-            font-family: helvetica, verdana;
-            background-color: #FFE5DB;
-        }
-    </style>
-</head>
-<body>
-      <h1>Oops! On dirait qu'il y a une erreur...</h1>
-      <p>Vous pouvez demander de l'aide sur <a href="http://groups.google.com/group/piwam">http://groups.google.com/group/piwam</a>.</p>
-      <p>Le serveur a retourné l'erreur suivante :</p>
-      <pre>
-        <?php echo $exception ?>
-      </pre>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="fr" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Piwam : Une erreur ça arrive toujours...</title>
+        <style type="text/css">
+            body {
+                font-family: helvetica, verdana, arial;
+                font-size: 1em;
+            }
 
-</body>
+            div#main {
+                padding-top: 50px;
+                margin: auto;
+                width: 500px;
+            }
+
+            h1 {
+                background-color: #FF5340;
+                color: white;
+                padding: 5px;
+                font-size: 2.4em;
+            }
+
+            .verbatim {
+                font-family: courier;
+                color: #999;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="main">
+            <h1>Oups, on dirait qu'il y a une erreur...</h1>
+
+            <?php if (get_class($exception) == 'sfFileException'): ?>
+                <?php
+                    $folder = getUnwritableFolder($exception->getMessage())
+                ?>
+                <p>
+                    Votre répertoire <span class="verbatim"><?php echo $folder ?>
+                    </span> n'est pas inscriptible. Avec votre client FTP,
+                    attribuez des droits en écriture sur le répertoire
+                    <span class="verbatim"><?php echo $folder ?></span>.
+                </p>
+                <p>
+                    En ligne de commande, cela correspond à la ligne suivante :
+                </p>
+                <p class="verbatim">
+                    &gt; user@host: chmod 777 <?php echo $folder ?>
+                </p>
+            <?php else: ?>
+                <p>
+                    L'erreur suivante est survenue :<br /><br />
+                    Message : <span class="verbatim"><?php echo $exception->getMessage() ?></span><br /><br />
+                    Détails : <span class="verbatim"><?php echo nl2br($exception) ?></span>
+                </p>
+            <?php endif; ?>
+            <p>
+                Vous pouvez aussi demander de l'aide sur le
+                <a href="http://groups.google.fr/group/piwam">groupe du projet</a>.
+            </p>
+        </div>
+    </body>
 </html>
