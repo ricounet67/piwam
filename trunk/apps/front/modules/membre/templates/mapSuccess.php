1e3fd2e1e44b88ab614f7dcd65bd2dfdb517359a
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-10-15 18:02:51 +0000

    Fixes a bug when retrieving the Google Map API key

--- trunk/apps/front/modules/membre/templates/mapSuccess.php
+++ trunk/apps/front/modules/membre/templates/mapSuccess.php
@@ -1,5 +1,5 @@
 <h2>Localisation des membres</h2>
 <?php
-use_javascript("http://maps.google.com/maps?file=api&v=2&key=" . sfConfig::get('sf_googlemap_key'));
+use_javascript("http://maps.google.com/maps?file=api&v=2&key=" . sfConfig::get('sf_googlemap_key', $associationId));
 $map->showMap();
 ?>
