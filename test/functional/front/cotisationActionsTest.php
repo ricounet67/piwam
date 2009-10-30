ca07173df24e00e504bb799aaf32b649b170c93d
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-10-30 21:42:30 +0000

    cotisation module has been refactored and tested.

--- test/functional/front/cotisationActionsTest.php
+++ test/functional/front/cotisationActionsTest.php
@@ -2,18 +2,38 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new sfGuardTestFunctional(new sfBrowser('docbook'));
+
+// Inputs
+$empty = array();
 
 $browser->
+info('Access to the index page which displays list')->
 get('/cotisation/index')->
-
-with('request')->begin()->
-isParameter('module', 'cotisation')->
-isParameter('action', 'index')->
+with('response')->begin()->
+    isStatusCode(200)->
+    checkElement('h2', 'Liste des cotisations')->
+    checkElement('p', '/Aucun type de cotisation/')->
 end()->
 
+
+
+info('Add a new empty cotisation, no existing type')->
+get('/cotisation/new')->
 with('response')->begin()->
-isStatusCode(200)->
-checkElement('body', '!/This is a temporary page/')->
-end()
+    isStatusCode(200)->
+    click('Sauvegarder', array('cotisation' => $empty))->
+end()->
+with('form')->begin()->
+    hasErrors(true)->
+end()->
+with('response')->begin()->
+    checkElement('.error_list', 2)->
+    checkElement('body', '/Requis/')->
+end()->
+
+
+
+info('Add a new cotisation type')
+
 ;
