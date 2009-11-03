13d5ffe2cfdb8cd7b1011e876856d115525a83e7
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-03 20:40:05 +0000

    Delete link is displayed when listing activities.

--- apps/front/modules/activite/templates/indexSuccess.php
+++ apps/front/modules/activite/templates/indexSuccess.php
@@ -15,10 +15,13 @@
         <tr>
             <td><?php echo $activite->getLibelle() ?></td>
             <td><?php echo format_date($activite->getCreatedAt()) ?></td>
-            <td><a
-                href="<?php echo url_for('activite/show?id=' . $activite->getId()) ?>"><?php echo image_tag('details.png', array('alt' => '[détails]')) ?></a>
-            <a
-                href="<?php echo url_for('activite/edit?id=' . $activite->getId()) ?>"><?php echo image_tag('edit.png', array('alt' => '[modifier]')) ?></a>
+            <td>
+                <a href="<?php echo url_for('activite/show?id=' . $activite->getId()) ?>"><?php echo image_tag('details.png', array('alt' => '[détails]')) ?></a>
+                <a href="<?php echo url_for('activite/edit?id=' . $activite->getId()) ?>"><?php echo image_tag('edit.png', array('alt' => '[modifier]')) ?></a>
+                <?php echo link_to(image_tag('delete'), 'activite/delete?id=' . $activite->getId(), array(
+                    'method'  => 'delete',
+                    'confirm' => 'Etes vous sûr ? Les recettes et dépenses associées seront aussi supprimées'
+                )) ?>
             </td>
         </tr>
         <?php endforeach; ?>
