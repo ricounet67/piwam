4570b62a3509d8a19f81db8e5e0946cf42c283c6
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-12 09:23:42 +0000

    piwam-install.sql is up to date

--- trunk/doc/piwam-install.sql
+++ trunk/doc/piwam-install.sql
@@ -736,7 +736,13 @@ ALTER TABLE `piwam_membre` CHANGE `pseudo` `pseudo` VARCHAR( 255 ) CHARACTER SET
 -- r190
 --
 ALTER TABLE `piwam_membre` CHANGE `nom` `nom` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
- 
+
+--
+-- r205
+--
+
+ALTER TABLE `piwam_cotisation_type` CHANGE `mis_a_jour_par` `mis_a_jour_par` INT( 11 ) NULL DEFAULT NULL ;
+
  --
  -- Think about updating the version !!
  --
