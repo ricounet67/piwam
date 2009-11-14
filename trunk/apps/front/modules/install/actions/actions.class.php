c81f75c60015e5d1076c6f543b0797daf813ce0e
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-14 18:39:16 +0000

    Non blocked installation if trombinoscope folder is not writable

--- trunk/apps/front/modules/install/actions/actions.class.php
+++ trunk/apps/front/modules/install/actions/actions.class.php
@@ -120,7 +120,7 @@ class installActions extends sfActions
     {
         $this->_addMessage(is_writable('../cache'),                 'isCacheFolderWritable');
         $this->_addMessage(is_writable('../log'),                   'isLogFolderWritable');
-        $this->_addMessage(is_writable('../web/uploads/trombinoscope/'), 'isTrombinoscopeFolderWritable');
+        $this->_addMessage(is_writable('../web/uploads/trombinoscope/'), 'isTrombinoscopeFolderWritable', true);
         $this->_addMessage(is_writable('../config/databases.yml'),  'isDatabasesFileWritable');
         $this->_addMessage(extension_loaded('openssl'),             'isPhpOpenSSLLoaded',       true);
         $this->_addMessage($this->_checkMemoryLimit('128M'),        'isMemoryLimitHighEnough',  true);
