c2c92f9fe2c1ac9836567909c442b55107d948d8
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-14 18:24:19 +0000

    Search bar get a custom CSS style

--- apps/front/modules/membre/templates/_searchForm.php
+++ apps/front/modules/membre/templates/_searchForm.php
@@ -4,7 +4,9 @@
  */
 ?>
 
-<form action="<?php echo url_for('membre/search'); ?>" method="post">
-    <?php echo $form->renderHiddenFields() ?>
-    <?php echo $form['magic'] ?> <input type="submit" name="submit" value="rechercher" class="button" />
-</form>
+<div id="searchBar">
+    <form action="<?php echo url_for('membre/search'); ?>" method="post">
+        <?php echo $form->renderHiddenFields() ?>
+        <?php echo $form['magic'] ?> <input type="submit" name="submit" value="rechercher" class="button" />
+    </form>
+</div>
