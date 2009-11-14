f72b77c0db4ab8603f3e997e637cc0844025df18
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-14 21:21:48 +0000

    Mini webservice is added to the web directory

---
+++ trunk/web/piwamversionner.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Mini web-service which is able to quickly say if Piwam is up to date
+ * or not according to the $_GET['current'] which referes to the current
+ * version of Piwam
+ *
+ */
+
+define('REQUEST_KEY', 'Piwam');
+
+if ((! isset($_GET['auth'])) || ($_GET['auth'] != REQUEST_KEY))
+{
+    echo "Erreur d'authentification";
+    exit ;
+}
+
+if (! isset($_GET['current']))
+{
+    echo "Erreur de paramètres";
+    exit ;
+}
+
+if ($_GET['current'] == '112')
+{
+    echo 'Version à jour';
+}
+?>
