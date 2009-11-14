6fc65069a3b0c71acf8924c5fb9c75046cf23f13
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-14 18:13:49 +0000

    recetteActionsTest is OK

--- test/functional/front/recetteActionsTest.php
+++ test/functional/front/recetteActionsTest.php
@@ -5,10 +5,10 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 $browser = new sfGuardTestFunctional(new sfBrowser());
 
 // Inputs
-$recette_empty			= array();
-$recette_wrong_montant	= array('libelle' => "Vente d'eau", 'montant' => '21a');
-$recette_ok				= array('libelle' => "Vente d'eau", 'montant' => '43.21');
-$creance_ok				= array('libelle' => "Vente d'eau", 'montant' => '43.21', 'percue' => '0');
+$recette_empty          = array();
+$recette_wrong_montant  = array('libelle' => "Vente d'eau", 'montant' => '21a');
+$recette_ok             = array('libelle' => "Vente d'eau", 'montant' => '43.21');
+$creance_ok             = array('libelle' => "Vente d'eau", 'montant' => '43.21', 'percue' => '0');
 
 
 $browser->
@@ -16,8 +16,8 @@ $browser->
 info("Acces a la liste des recettes")->
 get('/recette/index')->
 with('request')->begin()->
-isParameter('module', 'recette')->
-isParameter('action', 'index')->
+    isParameter('module', 'recette')->
+    isParameter('action', 'index')->
 end()->
 
 
@@ -25,32 +25,32 @@ end()->
 info("Ajout d'une recette vide")->
 get('/recette/new')->
 with('request')->begin()->
-isParameter('module', 'recette')->
-isParameter('action', 'new')->
+    isParameter('module', 'recette')->
+    isParameter('action', 'new')->
 end()->
 with('response')->begin()->
-click('Sauvegarder', array('recette' => $recette_empty))->
+    click('Sauvegarder', array('recette' => $recette_empty))->
 end()->
 with('form')->begin()->
-hasErrors(true)->
-isError('libelle', 'required')->
-isError('montant', 'required')->
+    hasErrors(true)->
+    isError('libelle', 'required')->
+    isError('montant', 'required')->
 end()->
 with('request')->begin()->
-isParameter('module', 'recette')->
-isParameter('action', 'create')->
+    isParameter('module', 'recette')->
+    isParameter('action', 'create')->
 end()->
 
 
 
 info("Ajout d'une recette avec montant invlaide")->
 with('response')->begin()->
-click('Sauvegarder', array('recette' => $recette_wrong_montant))->
+    click('Sauvegarder', array('recette' => $recette_wrong_montant))->
 end()->
 with('form')->begin()->
-hasErrors(true)->
-isError('libelle', false)->
-isError('montant', 'invalid')->
+    hasErrors(true)->
+    isError('libelle', false)->
+    isError('montant', 'invalid')->
 end()->
 
 
@@ -58,33 +58,33 @@ end()->
 
 info("Ajout d'une recette correcte")->
 with('response')->begin()->
-click('Sauvegarder', array('recette' => $recette_ok))->
+    click('Sauvegarder', array('recette' => $recette_ok))->
 end()->
 followRedirect()->
 with('request')->begin()->
-isParameter('module', 'recette')->
-isParameter('action', 'index')->
+    isParameter('module', 'recette')->
+    isParameter('action', 'index')->
 end()->
 with('response')->begin()->
-contains("Vente d&#039;eau")->
-contains("43,21")->
-contains("CAISSE_MONNAIE")->
+    contains("Vente d&#039;eau")->
+    contains("43,21")->
+    contains("CAISSE_MONNAIE")->
 end()->
 
 
 
 info("Acces aux details de la recette")->
 with('response')->begin()->
-click('[détails]')->
+    click('[détails]')->
 end()->
 with('response')->begin()->
-click('Retour')->
+    click('Retour')->
 end()->
 
 
 
 info("Acces a l'edition de la recette")->
 with('response')->begin()->
-click('[éditer]')->
+    click('[modifier]')->
 end()
 ;
