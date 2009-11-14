7f56d4f0f1595b204d86fb121029bb44089c7b88
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-14 20:40:53 +0000

    Update module catch exception if SQL_DIR is not valid

--- trunk/apps/front/modules/update/templates/indexSuccess.php
+++ trunk/apps/front/modules/update/templates/indexSuccess.php
@@ -7,19 +7,31 @@
     </tr>
     <tr>
         <th>Fichiers SQL à exécuter :</th>
-        <td><?php if (count($files) === 0): ?> Aucun fichier à exécuter <?php else: ?>
-        <ul>
-        <?php foreach ($files as $file): ?>
-            <li><pre><?php echo $file ?></pre></li>
-            <?php endforeach; ?>
-        </ul>
-        <?php endif ?></td>
-    </tr>
-    <?php if (count($files) !== 0): ?>
-    <tr>
-        <th>&nbsp;</th>
-        <td><?php echo link_to('Exécuter', 'update/perform', array('class' => 'button')) ?>
-        </td>
-    </tr>
-    <?php endif ?>
+
+        <?php if (isset($error)): ?>
+            <td>
+                Erreur : <?php echo $error ?>
+            </td>
+        <?php else: ?>
+            <td>
+                <?php if (count($files) === 0): ?>
+                    Aucun fichier à exécuter
+                <?php else: ?>
+                    <ul>
+                        <?php foreach ($files as $file): ?>
+                            <li><pre><?php echo $file ?></pre></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif ?>
+            </td>
+        </tr>
+
+        <?php if (count($files) !== 0): ?>
+            <tr>
+                <th>&nbsp;</th>
+                <td><?php echo link_to('Exécuter', 'update/perform', array('class' => 'button')) ?>
+                </td>
+            </tr>
+        <?php endif ?>
+    <?php endif?>
 </table>
