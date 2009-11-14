11314383212572492b672f0b8b42263759df7692
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-14 11:22:07 +0000

    MembrePeer implements a buildCriteria method to build a critera after filtering parameters

--- trunk/apps/front/modules/membre/templates/_searchForm.php
+++ trunk/apps/front/modules/membre/templates/_searchForm.php
@@ -6,5 +6,5 @@
 
 <form action="<?php echo url_for('membre/search'); ?>" method="post">
     <?php echo $form->renderHiddenFields() ?>
-    <?php echo $form['query'] ?> <input type="submit" name="submit" value="rechercher" />
+    <?php echo $form['magic'] ?> <input type="submit" name="submit" value="rechercher" />
 </form>
