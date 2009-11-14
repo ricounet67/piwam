b789c2f8c73b59e2f9fc3969376f551b24b1ca5e
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-14 22:00:08 +0000

    Mailing action has been put in mailing module

--- trunk/apps/front/modules/mailing/actions/actions.class.php
+++ trunk/apps/front/modules/mailing/actions/actions.class.php
@@ -5,18 +5,60 @@
  *
  * @package    piwam
  * @subpackage mailing
- * @author     Your name here
+ * @author     Adrien Mogenet
  * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
  */
 class mailingActions extends sfActions
 {
     /**
-     * Executes index action
+     * Executes Index action. Send email to each member if form has been
+     * submit.
      *
      * @param sfRequest $request A request object
      */
     public function executeIndex(sfWebRequest $request)
     {
-        $this->forward('default', 'module');
+        $this->form = new MailingForm(array(), array('url' => $this->getController()->genUrl('membre/ajaxlist')));
+        if ($request->isMethod('post'))
+        {
+            $this->form->bind($request->getParameter('mailing'));
+            if ($this->form->isValid())
+            {
+                $associationId = $this->getUser()->getAssociationId();
+                $data   = $this->form->getValues();
+                $sentOk = 0;    // these are 2 counters of
+                $sentKo = 0;    // succeed/failed messages
+
+                try
+                {
+                    $mailer   = MailerFactory::get($associationId, $this->getUser());
+                    $message  = new Swift_Message($data['subject'], $data['mail_content'], 'text/html');
+                    $from     = Configurator::get('address', $associationId, 'info-association@piwam.org');
+                    $membres  = MembrePeer::doSelectWithEmailForAssociation($this->getUser()->getAssociationId());
+
+                    foreach ($membres as $membre)
+                    {
+                        try
+                        {
+                            $mailer->send($message, $membre->getEmail(), $from);
+                            $sentOk++;
+                        }
+                        catch(Swift_ConnectionException $e)
+                        {
+                            $sentKo++;
+                        }
+                    }
+
+                    $mailer->disconnect();
+                    sfContext::getInstance()->getConfiguration()->loadHelpers('Plural');
+                    $this->getUser()->setFlash('notice', 'Votre message a été envoyé à ' . $sentOk . plural_word($sentOk, ' destinataire') . ' (' . $sentKo . plural_word($sentKo, ' erreur') . ')');
+                    $this->content = $data['mail_content'];
+                }
+                catch (Exception $e)
+                {
+                    //
+                }
+            }
+        }
     }
 }
---
+++ trunk/apps/front/modules/mailing/templates/indexSuccess.php
@@ -0,0 +1,54 @@
+<h2>Mailing</h2>
+
+<?php use_javascript('/sfFormExtraPlugin/js/jquery.autocompleter.js') ?>
+<?php use_stylesheet('/sfFormExtraPlugin/css/jquery.autocompleter.css') ?>
+
+
+<?php if ($sf_user->hasFlash('notice')): ?>
+<p class="notice"><?php echo $sf_user->getFlash('notice') ?></p>
+<?php endif; ?>
+
+<?php if ($sf_user->hasFlash('error')): ?>
+<p class="error"><?php echo image_tag('error', array('align' => 'top', 'alt' => 'Erreur')) ?> <strong>ERREUR</strong>
+: <?php echo $sf_user->getFlash('error') ?></p>
+<?php endif; ?>
+
+<?php  if (isset($content)): ?>
+<div class="mailPreview">
+<p><strong>Votre message :</strong>
+
+
+<hr />
+<?php echo html_entity_decode($content) ?></div>
+<?php else: ?>
+
+<form action="<?php echo url_for('mailing/index') ?>" method="POST">
+<table class="formArray">
+    <tr>
+        <td colspan="2"><?php
+        if ($form->hasErrors())
+        {
+            echo '<div class="error">ERREUR : Vous devez entrer un sujet et un message</div>';
+        }
+        ?></td>
+    </tr>
+    <tr>
+        <th>Objet</th>
+        <td><?php echo $form['subject'] ?></td>
+    </tr>
+    <tr>
+        <th>Destinataires</th>
+        <td>Tout le monde</td>
+    </tr>
+    <tr>
+        <th valign="top">Message</th>
+        <td><?php echo $form['mail_content'] ?></td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td><input type="submit" class="button" value="envoyer" /></td>
+    </tr>
+</table>
+</form>
+
+<?php endif; ?>
