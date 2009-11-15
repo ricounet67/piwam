a21a24b32a97b41d1747436f8edfe9c61d94aec4
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-15 20:00:50 +0000

    frontConfiguration class has been commented

--- trunk/apps/front/config/frontConfiguration.class.php
+++ trunk/apps/front/config/frontConfiguration.class.php
@@ -1,13 +1,22 @@
 <?php
-
+/**
+ * Configure some parameters for the front application
+ *
+ * @author adrien
+ */
 class frontConfiguration extends sfApplicationConfiguration
 {
+    /**
+     * Configure global constants and messages
+     *
+     * @see lib/vendor/symfony/1.2/lib/config/sfApplicationConfiguration#configure()
+     */
 	public function configure()
 	{
 		// Represents the states of `actif` fields
 		define('ENABLED',		1);
 		define('DISABLED',		0);
-		
+
 		// Symbol which is used as CSV separator when exporting files
 		define('CSV_SEPARATOR',	',');
 
