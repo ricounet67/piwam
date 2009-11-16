2a14007031f2c4409b7a0ecb2e1e88b7dfbbcc4a
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-16 08:44:28 +0000

    Mailing form is w3c compliant

--- apps/front/lib/form/mailingForm.class.php
+++ apps/front/lib/form/mailingForm.class.php
@@ -15,32 +15,28 @@ class MailingForm extends sfForm
         $this->setWidgets(array(
 			'subject'		=> new sfWidgetFormInput(),
 			'mail_content' 	=> new sfWidgetFormTextareaTinyMCE(array(
-			  'width'  => 550,
-			  'height' => 350,
-			  'config' => '	theme_advanced_buttons1 : "bold,italic,underline,fontsizeselect,fontselect,forecolorpicker,image,link,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,indent,outdent",
-			  				theme_advanced_buttons2 : "",
-			  				theme_advanced_buttons3 : "",
-			  				theme_advanced_statusbar_location : "none"'
-			  				))
-			  				));
+                    			  'width'  => 550,
+                    			  'height' => 350,
+                    			  'config' => '	theme_advanced_buttons1 : "bold,italic,underline,fontsizeselect,fontselect,forecolorpicker,image,link,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,indent,outdent",
+                    			  				theme_advanced_buttons2 : "",
+                    			  				theme_advanced_buttons3 : "",
+                    			  				theme_advanced_statusbar_location : "none"'
+			  				   ),
+			  				   array('rows' => 40, 'cols' => 10)),
+		));
 
-			  				$this->setValidators(array(
-			'subject'		=> new sfValidatorString(array('required' => true)),
-			'mail_content'	=> new sfValidatorString(array('required' => true)),
-			  				));
+    	$this->setValidators(array(
+            'subject'		=> new sfValidatorString(array('required' => true)),
+            'mail_content'	=> new sfValidatorString(array('required' => true)),
+    	));
 
-			  				$this->widgetSchema->setNameFormat('mailing[%s]');
-			  				$this->widgetSchema['subject']->setAttribute('class', 'formInputXtraLarge');
+    	$this->widgetSchema->setLabels(array(
+            'subject'       => 'Objet',
+    	    'mail_content'  => 'Message',
+    	));
 
-
-			  				// r19 : autocompleted recipients field
-			  				//       it's currently not displayed
-
-			  				$this->widgetSchema['recipients'] = new sfWidgetFormPropelJQueryAutocompleter(array(
-		  	'model' => 'Membre',
-  			'url'   => $this->getOption('url'),
-			  				));
-			  				$this->widgetSchema['recipients']->setAttribute('class', 'formInputNormal');
+    	$this->widgetSchema->setNameFormat('mailing[%s]');
+    	$this->widgetSchema['subject']->setAttribute('class', 'formInputXtraLarge');
     }
 }
 ?>
