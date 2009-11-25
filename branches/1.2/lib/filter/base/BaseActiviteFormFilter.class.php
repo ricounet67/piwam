9d3a55edc44984f8629abfea7e2ec749290ba773
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-25 20:29:48 +0000

    [1.2] Propel model has been rebuilt

--- branches/1.2/lib/filter/base/BaseActiviteFormFilter.class.php
+++ branches/1.2/lib/filter/base/BaseActiviteFormFilter.class.php
@@ -1,30 +1,28 @@
 <?php
 
-require_once(sfConfig::get('sf_lib_dir').'/filter/base/BaseFormFilterPropel.class.php');
-
 /**
  * Activite filter form base class.
  *
  * @package    piwam
  * @subpackage filter
- * @author     Your name here
- * @version    SVN: $Id: sfPropelFormFilterGeneratedTemplate.php 16976 2009-04-04 12:47:44Z fabien $
+ * @author     Adrien Mogenet
+ * @version    SVN: $Id: sfPropelFormFilterGeneratedTemplate.php 24051 2009-11-16 21:08:08Z Kris.Wallsmith $
  */
-class BaseActiviteFormFilter extends BaseFormFilterPropel
+abstract class BaseActiviteFormFilter extends BaseFormFilterPropel
 {
-    public function setup()
-    {
-        $this->setWidgets(array(
-      'libelle'        => new sfWidgetFormFilterInput(),
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'libelle'        => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'actif'          => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
       'association_id' => new sfWidgetFormPropelChoice(array('model' => 'Association', 'add_empty' => true)),
       'enregistre_par' => new sfWidgetFormPropelChoice(array('model' => 'Membre', 'add_empty' => true)),
       'mis_a_jour_par' => new sfWidgetFormPropelChoice(array('model' => 'Membre', 'add_empty' => true)),
-      'created_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
-      'updated_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
-        ));
+      'created_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
+      'updated_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
+    ));
 
-        $this->setValidators(array(
+    $this->setValidators(array(
       'libelle'        => new sfValidatorPass(array('required' => false)),
       'actif'          => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
       'association_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Association', 'column' => 'id')),
@@ -32,23 +30,23 @@ class BaseActiviteFormFilter extends BaseFormFilterPropel
       'mis_a_jour_par' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Membre', 'column' => 'id')),
       'created_at'     => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
       'updated_at'     => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
-        ));
+    ));
 
-        $this->widgetSchema->setNameFormat('activite_filters[%s]');
+    $this->widgetSchema->setNameFormat('activite_filters[%s]');
 
-        $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
 
-        parent::setup();
-    }
+    parent::setup();
+  }
 
-    public function getModelName()
-    {
-        return 'Activite';
-    }
+  public function getModelName()
+  {
+    return 'Activite';
+  }
 
-    public function getFields()
-    {
-        return array(
+  public function getFields()
+  {
+    return array(
       'id'             => 'Number',
       'libelle'        => 'Text',
       'actif'          => 'Boolean',
@@ -57,6 +55,6 @@ class BaseActiviteFormFilter extends BaseFormFilterPropel
       'mis_a_jour_par' => 'ForeignKey',
       'created_at'     => 'Date',
       'updated_at'     => 'Date',
-        );
-    }
+    );
+  }
 }
--- branches/1.2/lib/filter/base/BaseRecetteFormFilter.class.php
+++ branches/1.2/lib/filter/base/BaseRecetteFormFilter.class.php
@@ -1,34 +1,32 @@
 <?php
 
-require_once(sfConfig::get('sf_lib_dir').'/filter/base/BaseFormFilterPropel.class.php');
-
 /**
  * Recette filter form base class.
  *
  * @package    piwam
  * @subpackage filter
- * @author     Your name here
- * @version    SVN: $Id: sfPropelFormFilterGeneratedTemplate.php 16976 2009-04-04 12:47:44Z fabien $
+ * @author     Adrien Mogenet
+ * @version    SVN: $Id: sfPropelFormFilterGeneratedTemplate.php 24051 2009-11-16 21:08:08Z Kris.Wallsmith $
  */
-class BaseRecetteFormFilter extends BaseFormFilterPropel
+abstract class BaseRecetteFormFilter extends BaseFormFilterPropel
 {
-    public function setup()
-    {
-        $this->setWidgets(array(
-      'libelle'        => new sfWidgetFormFilterInput(),
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'libelle'        => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'association_id' => new sfWidgetFormPropelChoice(array('model' => 'Association', 'add_empty' => true)),
-      'montant'        => new sfWidgetFormFilterInput(),
+      'montant'        => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'compte_id'      => new sfWidgetFormPropelChoice(array('model' => 'Compte', 'add_empty' => true)),
       'activite_id'    => new sfWidgetFormPropelChoice(array('model' => 'Activite', 'add_empty' => true)),
       'date'           => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'percue'         => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
       'enregistre_par' => new sfWidgetFormPropelChoice(array('model' => 'Membre', 'add_empty' => true)),
       'mis_a_jour_par' => new sfWidgetFormPropelChoice(array('model' => 'Membre', 'add_empty' => true)),
-      'created_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
-      'updated_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => true)),
-        ));
+      'created_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
+      'updated_at'     => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
+    ));
 
-        $this->setValidators(array(
+    $this->setValidators(array(
       'libelle'        => new sfValidatorPass(array('required' => false)),
       'association_id' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Association', 'column' => 'id')),
       'montant'        => new sfValidatorSchemaFilter('text', new sfValidatorNumber(array('required' => false))),
@@ -40,23 +38,23 @@ class BaseRecetteFormFilter extends BaseFormFilterPropel
       'mis_a_jour_par' => new sfValidatorPropelChoice(array('required' => false, 'model' => 'Membre', 'column' => 'id')),
       'created_at'     => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
       'updated_at'     => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))),
-        ));
+    ));
 
-        $this->widgetSchema->setNameFormat('recette_filters[%s]');
+    $this->widgetSchema->setNameFormat('recette_filters[%s]');
 
-        $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
 
-        parent::setup();
-    }
+    parent::setup();
+  }
 
-    public function getModelName()
-    {
-        return 'Recette';
-    }
+  public function getModelName()
+  {
+    return 'Recette';
+  }
 
-    public function getFields()
-    {
-        return array(
+  public function getFields()
+  {
+    return array(
       'id'             => 'Number',
       'libelle'        => 'Text',
       'association_id' => 'ForeignKey',
@@ -69,6 +67,6 @@ class BaseRecetteFormFilter extends BaseFormFilterPropel
       'mis_a_jour_par' => 'ForeignKey',
       'created_at'     => 'Date',
       'updated_at'     => 'Date',
-        );
-    }
+    );
+  }
 }
