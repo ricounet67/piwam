4412bb7ec02187ca0fd367ba035c10de62794b1b
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 16:15:43 +0000

    [1.2] AclCredentialTable::getForMember() has been implementd

--- lib/model/doctrine/AclCredentialTable.class.php
+++ lib/model/doctrine/AclCredentialTable.class.php
@@ -1,6 +1,27 @@
 <?php
-
+/**
+ * Implements operations on AclCredential table (credentials that are
+ * given to a Member)
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @author      Adrien Mogenet
+ * @since       1.2
+ */
 class AclCredentialTable extends Doctrine_Table
 {
+  /**
+   * Retrieve Credentials for member $id
+   *
+   * @param   integer   $id
+   * @return  array of AclCredential
+   */
+  public static function getForMember($id)
+  {
+    $q = Doctrine_Query::create()
+          ->from('AclCredential c')
+          ->where('c.member_id = ?', $id);
 
+    return $q->fetchArray();
+  }
 }
