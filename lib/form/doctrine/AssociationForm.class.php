50b655d412cba695393f005b209a9c0377628a3d
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 14:51:09 +0000

    [1.2] AssociationForm is well indented

--- lib/form/doctrine/AssociationForm.class.php
+++ lib/form/doctrine/AssociationForm.class.php
@@ -10,38 +10,38 @@
  */
 class AssociationForm extends BaseAssociationForm
 {
-    public function configure()
+  public function configure()
+  {
+    unset($this['created_at'], $this['updated_at']);
+    unset($this['created_by'], $this['updated_by']);
+    unset($this['state'], $this['association_id']);
+    unset($this['description']);
+
+    if (! $this->getObject()->isNew())
     {
-        unset($this['created_at'], $this['updated_at']);
-        unset($this['created_by'], $this['updated_by']);
-        unset($this['state'], $this['association_id']);
-        unset($this['description']);
-
-        if (! $this->getObject()->isNew())
-        {
-            $this->widgetSchema['updated_by'] = new sfWidgetFormInputHidden();
-            $this->setDefault('updated_by', sfContext::getInstance()->getUser()->getAttribute('user_id', null, 'user'));
-            $this->validatorSchema['updated_by'] = new sfValidatorInteger(array('required' => false));
-        }
-        else
-        {
-            $this->widgetSchema['ping_piwam'] = new sfWidgetFormInputCheckbox();
-            $this->setDefault('ping_piwam', true);
-            $this->validatorSchema['ping_piwam'] = new sfValidatorBoolean();
-        }
-
-        $this->widgetSchema['description'] = new sfWidgetFormTextarea();
-        $this->validatorSchema['description'] = new sfValidatorString(array('required' => false));
-
-        $this->widgetSchema['state'] = new sfWidgetFormInputHidden();
-        $this->setDefault('state', AssociationTable::STATE_ENABLED);
-
-        $this->validatorSchema['website'] = new sfValidatorUrl(array('required' => false));
-        $this->validatorSchema['state'] = new sfValidatorBoolean();
-
-        // r15 : set length of fields
-        $this->widgetSchema['name']->setAttribute('class', 'formInputLarge');
-        $this->widgetSchema['description']->setAttribute('class', 'formInputLarge');
-        $this->widgetSchema['website']->setAttribute('class', 'formInputLarge');
+      $this->widgetSchema['updated_by'] = new sfWidgetFormInputHidden();
+      $this->setDefault('updated_by', sfContext::getInstance()->getUser()->getAttribute('user_id', null, 'user'));
+      $this->validatorSchema['updated_by'] = new sfValidatorInteger(array('required' => false));
     }
+    else
+    {
+      $this->widgetSchema['ping_piwam'] = new sfWidgetFormInputCheckbox();
+      $this->setDefault('ping_piwam', true);
+      $this->validatorSchema['ping_piwam'] = new sfValidatorBoolean();
+    }
+
+    $this->widgetSchema['description'] = new sfWidgetFormTextarea();
+    $this->validatorSchema['description'] = new sfValidatorString(array('required' => false));
+
+    $this->widgetSchema['state'] = new sfWidgetFormInputHidden();
+    $this->setDefault('state', AssociationTable::STATE_ENABLED);
+
+    $this->validatorSchema['website'] = new sfValidatorUrl(array('required' => false));
+    $this->validatorSchema['state'] = new sfValidatorBoolean();
+
+    // r15 : set length of fields
+    $this->widgetSchema['name']->setAttribute('class', 'formInputLarge');
+    $this->widgetSchema['description']->setAttribute('class', 'formInputLarge');
+    $this->widgetSchema['website']->setAttribute('class', 'formInputLarge');
+  }
 }
