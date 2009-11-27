117e34df36fdbf898ad4918b786e5110289b82c0
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 08:51:07 +0000

    [1.2] AssociationTable implements doCount()

--- branches/1.2/lib/model/doctrine/AssociationTable.class.php
+++ branches/1.2/lib/model/doctrine/AssociationTable.class.php
@@ -1,15 +1,26 @@
 <?php
-
+/**
+ * Implements operations on Association table
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @author      Adrien Mogenet
+ * @since       1.2
+ */
 class AssociationTable extends Doctrine_Table
 {
+    /**
+     * Count existing associations
+     *
+     * @return  integer
+     */
     public static function doCount()
     {
         $q = Doctrine_Query::create()
                 ->select('count(a.id) AS n')
                 ->from('Association a')
-                ->groupBy('a.id')
-                ->execute();
+                ->groupBy('a.id');
 
-        return $q[0];
+        return $q->count();
     }
 }
