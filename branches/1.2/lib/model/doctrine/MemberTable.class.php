36cb077dbc2a0744a11e2d092fddc60e8e4d28af
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 08:24:29 +0000

    [1.2 MemberTable implements getPagerOrderBy and getPending

--- branches/1.2/lib/model/doctrine/MemberTable.class.php
+++ branches/1.2/lib/model/doctrine/MemberTable.class.php
@@ -8,20 +8,75 @@
 class MemberTable extends Doctrine_Table
 {
   /**
+   * Value of state if user account is disabled
+   *
+   * @var integer
+   */
+  const STATE_DISABLED  = 0;
+
+  /**
+   * Value of state if user account is enabled
+   *
+   * @var integer
+   */
+  const STATE_ENABLED   = 1;
+
+  /**
+   * Value of state if user account is pending
+   *
+   * @var integer
+   */
+  const STATE_PENDING   = 2;
+
+  /**
    * Get the list of active members who belong to the
    * association $id
    *
-   * @param   integer         $id
-   * @return  array of Membre
+   * @param   integer           $association_id
+   * @param   integer           $page
+   * @param   string            $column
+   * @return  sfDoctrinePager
    */
-  public static function getForAssociation($id)
+  public static function getPagerOrderBy($association_id, $page = 1, $column = 'lastname')
   {
     $q = Doctrine_Query::create()
           ->select('m.*')
           ->from('Member m')
-          ->where('m.association_id = ?', $id)
-          ->execute();
+          ->where('m.association_id = ?', $association_id)
+          ->andWhere('m.state = ?', self::STATE_ENABLED);
 
-    return $q;
+    $pager = new sfDoctrinePager('Member', 20);
+    $pager->setQuery($q);
+    $pager->setPage($page);
+
+    return $pager;
+  }
+
+  /**
+   * Retrieve pending user accounts
+   *
+   * @param   integer           $association_id
+   * @return  array of Member
+   */
+  public static function getPendingMembers($association_id)
+  {
+    $q = Doctrine_Query::create()
+          ->select('m.*')
+          ->from('Member m')
+          ->where('m.association_id = ?', $association_id)
+          ->andWhere('state = ?', self::STATE_PENDING)
+          ->fetchArray();
+  }
+
+  /**
+   * Try to select users matching $username and $password.
+   *
+   * @param   string    $username
+   * @param   string    $password
+   * @return  Member
+   */
+  public static function getByUsernameAndPassword($username, $password)
+  {
+
   }
 }
