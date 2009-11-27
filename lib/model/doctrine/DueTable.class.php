cdcb4b6962b9e677fbd763d3215427ff22ebf688
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 16:26:49 +0000

    [1.2] DueTable::getForUser() has been implementd

--- lib/model/doctrine/DueTable.class.php
+++ lib/model/doctrine/DueTable.class.php
@@ -1,6 +1,26 @@
 <?php
-
+/**
+ * Implements operations on Due table
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @since       1.2
+ * @author      adrien
+ */
 class DueTable extends Doctrine_Table
 {
+  /**
+   * Retrieve Dues paid by member $id
+   *
+   * @param   integer $id
+   * @return  array of Due
+   */
+  public static function getForUser($id)
+  {
+    $q = Doctrine_Query::create()
+          ->from('Due d')
+          ->where('d.member_id = ?', $id);
 
+    return $q->fetchArray();
+  }
 }
