dc6b1e3c33d89137514fa37cf2ef9bf450e2317a
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 14:39:03 +0000

    [1.2] Constants representing states of row in ActivityTable

--- lib/model/doctrine/ActivityTable.class.php
+++ lib/model/doctrine/ActivityTable.class.php
@@ -1,6 +1,25 @@
 <?php
-
+/**
+ * Implements operations of activity table
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @since       1.2
+ * @author      Adrien Mogenet <adrien.mogenet@gmail.com
+ */
 class ActivityTable extends Doctrine_Table
 {
+    /**
+     * Value of state field when disabled
+     *
+     * @var integer
+     */
+    const STATE_DISABLED    = 0;
 
+    /**
+     * Value of state field when enabled
+     *
+     * @var integer
+     */
+    const STATE_ENABLED     = 1;
 }
