24d9a148969340ef20190ceee89e4a6ea87ce5fd
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 14:42:42 +0000

    [1.2] Constants representing states in AccountTable

--- branches/1.2/lib/model/doctrine/AccountTable.class.php
+++ branches/1.2/lib/model/doctrine/AccountTable.class.php
@@ -1,6 +1,25 @@
 <?php
-
+/**
+ * Implements operations on Account table
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @since       1.2
+ * @author      Adrien Mogenet
+ */
 class AccountTable extends Doctrine_Table
 {
+  /**
+   * Value of state field when disabled
+   *
+   * @var integer
+   */
+  const STATE_DISABLED    = 0;
 
+  /**
+   * Value of state field when enabled
+   *
+   * @var integer
+   */
+  const STATE_ENABLED     = 1;
 }
