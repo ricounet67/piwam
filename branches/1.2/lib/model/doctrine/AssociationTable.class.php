741be25f29cd2cd4447da1127fa8c974f402f07e
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-27 15:32:32 +0000

    [1.2] AssociationTable implements getById()

--- branches/1.2/lib/model/doctrine/AssociationTable.class.php
+++ branches/1.2/lib/model/doctrine/AssociationTable.class.php
@@ -9,32 +9,49 @@
  */
 class AssociationTable extends Doctrine_Table
 {
-    /**
-     * Value of state field when disabled
-     *
-     * @var integer
-     */
-    const STATE_DISABLED    = 0;
+  /**
+   * Value of state field when disabled
+   *
+   * @var integer
+   */
+  const STATE_DISABLED    = 0;
 
-    /**
-     * Value of state field when enabled
-     *
-     * @var integer
-     */
-    const STATE_ENABLED     = 1;
+  /**
+   * Value of state field when enabled
+   *
+   * @var integer
+   */
+  const STATE_ENABLED     = 1;
 
-    /**
-     * Count existing associations
-     *
-     * @return  integer
-     */
-    public static function doCount()
-    {
-        $q = Doctrine_Query::create()
-                ->select('count(a.id) AS n')
-                ->from('Association a')
-                ->groupBy('a.id');
+  /**
+   * Retrieve an Association row by id
+   *
+   * @param   integer     $id
+   * @return  Association
+   */
+  public static function getById($id)
+  {
+    $q = Doctrine_Query::create()
+          ->select('a.id')
+          ->from('Association a')
+          ->where('id = ?', $id)
+          ->fetchOne();
 
-        return $q->count();
-    }
+    return $q;
+  }
+
+  /**
+   * Count existing associations
+   *
+   * @return  integer
+   */
+  public static function doCount()
+  {
+    $q = Doctrine_Query::create()
+          ->select('count(a.id) AS n')
+          ->from('Association a')
+          ->groupBy('a.id');
+
+    return $q->count();
+  }
 }
