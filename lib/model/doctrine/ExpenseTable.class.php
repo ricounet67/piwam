07ef58e72745cffab2c2d2001da9657570ac198a
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 19:14:41 +0000

    [1.2] ExpenseTable::getPagerForAssociation() has been implemented

--- lib/model/doctrine/ExpenseTable.class.php
+++ lib/model/doctrine/ExpenseTable.class.php
@@ -1,6 +1,34 @@
 <?php
-
+/**
+ * Implements operations on Expense table
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @author      Adrien Mogenet
+ * @since       1.2
+ */
 class ExpenseTable extends Doctrine_Table
 {
+  /**
+   * Retrieve enabled expenses for association $id
+   *
+   * @param   integer           $id
+   * @return  array of Expense
+   */
+  public static function getPagerForAssociation($id, $page = 1)
+  {
+    $q = Doctrine_Query::create()
+          ->from('Expense e')
+          ->where('e.association_id = ?', $id);
 
+    /**
+     * @todo
+     * FIXME
+     */
+    $pager = new sfDoctrinePager('Expense', 20);
+    $pager->setQuery($q);
+    $pager->setPage($page);
+
+    return $pager;
+  }
 }
