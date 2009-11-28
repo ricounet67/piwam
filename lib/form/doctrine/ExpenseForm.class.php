bd1ce6fcf03b2878d940a68ca4ddc2a786964741
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 20:21:04 +0000

    [1.2] ExpenseForm uses new field names

--- lib/form/doctrine/ExpenseForm.class.php
+++ lib/form/doctrine/ExpenseForm.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Expense form.
  *
@@ -10,7 +9,60 @@
  */
 class ExpenseForm extends BaseExpenseForm
 {
+  /**
+   * Customizes the Recette form. There is a lot of fields to unset in order
+   * to re-create them from scratch with custom behaviour, especially the
+   * hidden references (association, granted user id...)
+   */
   public function configure()
   {
+    unset($this['created_at'],    $this['updated_at']);
+    unset($this['created_by'],  $this['updated_by']);
+    unset($this['state'],       $this['association_id']);
+
+    if ($this->getObject()->isNew()) {
+      $this->widgetSchema['created_by'] = new sfWidgetFormInputHidden();
+      $this->widgetSchema['association_id'] = new sfWidgetFormInputHidden();
+      $this->setDefault('created_by', sfContext::getInstance()->getUser()->getAttribute('user_id', null, 'user'));
+      $this->setDefault('association_id', sfContext::getInstance()->getUser()->getAttribute('association_id', null, 'user'));
+      $this->validatorSchema['association_id'] = new sfValidatorInteger();
+      $this->validatorSchema['created_by'] = new sfValidatorInteger();
+    }
+
+    $this->widgetSchema['updated_by'] = new sfWidgetFormInputHidden();
+    $this->widgetSchema['state'] = new sfWidgetFormInputHidden();
+    $this->setDefault('state', 1);
+
+    $this->validatorSchema['updated_by'] = new sfValidatorInteger();
+    $this->validatorSchema['state'] = new sfValidatorBoolean();
+    $this->validatorSchema['amount'] = new sfValidatorAmount(array('min' => 0), array('min' => 'ne peut être négatif'));
+
+    // select only Membre, CotisationType and account which
+    // belong to the association id
+
+    $id = sfContext::getInstance()->getUser()->getAttribute('association_id', null, 'user');
+
+
+    /**
+     * @todo
+     * FIXME
+     */
+//    $this->widgetSchema['account_id']->setOption('criteria', accountPeer::getCriteriaForAssociationId($id));
+//    $this->widgetSchema['activity_id']->setOption('criteria', activityPeer::getCriteriaForAssociationId($id));
+
+    // r19 : customize the appearance
+    $this->widgetSchema['label']->setAttribute('class', 'formInputLarge');
+    $this->widgetSchema['amount']->setAttribute('class', 'formInputShort');
+
+    sfContext::getInstance()->getConfiguration()->loadHelpers("Asset");
+    $this->widgetSchema['date'] = new sfWidgetFormJQueryDate(array(
+      'image'   => image_path('calendar.gif'),
+        'config'  => '{}',
+      'culture' => 'fr_FR',
+      'format'  => '%day%.%month%.%year%',
+    ));
+    $this->setDefault('date', date('y-m-d'));
+    $this->widgetSchema['account_id']->setAttribute('class', 'formInputLarge');
+    $this->widgetSchema['activity_id']->setAttribute('class', 'formInputLarge');
   }
 }
