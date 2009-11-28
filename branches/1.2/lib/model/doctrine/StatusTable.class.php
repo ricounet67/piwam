68b7406bdefb300f510fb7c581200de605f354c1
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 21:16:39 +0000

    [1.2] StatusTable::getEnabledForAssociation() has been implemented

--- branches/1.2/lib/model/doctrine/StatusTable.class.php
+++ branches/1.2/lib/model/doctrine/StatusTable.class.php
@@ -9,18 +9,34 @@
  */
 class StatusTable extends Doctrine_Table
 {
-    /**
-     * Value of state field when disabled
-     *
-     * @var integer
-     */
-    const STATE_DISABLED    = 0;
+  /**
+   * Value of state field when disabled
+   *
+   * @var integer
+   */
+  const STATE_DISABLED    = 0;
 
-    /**
-     * Value of state field when enabled
-     *
-     * @var integer
-     */
-    const STATE_ENABLED     = 1;
+  /**
+   * Value of state field when enabled
+   *
+   * @var integer
+   */
+  const STATE_ENABLED     = 1;
+
+  /**
+   * Retrieve existing status for association $id
+   *
+   * @param   integer         $id
+   * @return  array of Status
+   */
+  public static function getEnabledForAssociation($id)
+  {
+    $q = Doctrine_Query::create()
+          ->from('Status s')
+          ->where('s.association_id = ?',  $id)
+          ->andWhere('s.state = ?', self::STATE_ENABLED);
+
+    return $q->execute();
+  }
 
 }
