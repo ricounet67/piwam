2a22469f2fa230350ada09c8f7b571f63f69dcc5
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 20:02:00 +0000

    [1.2] ConfigVariable::getByCode() has been implemented

--- lib/model/doctrine/ConfigVariableTable.class.php
+++ lib/model/doctrine/ConfigVariableTable.class.php
@@ -1,6 +1,26 @@
 <?php
-
+/**
+ * Implements operations on ConfigVariable table
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @author      Adrien Mogenet
+ * @since       1.2
+ */
 class ConfigVariableTable extends Doctrine_Table
 {
+  /**
+   * Get a ConfigVariable by its code
+   *
+   * @param   string  $code
+   * @return  ConfigVariable
+   */
+  public static function getByCode($code)
+  {
+    $q = Doctrine_Query::create()
+          ->from('ConfigVariable v')
+          ->where('v.code = ?', $code);
 
+    return $q->fetchOne();
+  }
 }
