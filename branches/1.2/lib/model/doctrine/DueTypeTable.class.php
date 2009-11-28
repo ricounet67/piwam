2ea0358269f06eeeb92fc9c8e119fd6378c85111
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 19:04:42 +0000

    [1.2] DueTypeTable::getAmountForType() has been implemented

--- branches/1.2/lib/model/doctrine/DueTypeTable.class.php
+++ branches/1.2/lib/model/doctrine/DueTypeTable.class.php
@@ -24,6 +24,21 @@ class DueTypeTable extends Doctrine_Table
   const STATE_ENABLED     = 1;
 
   /**
+   * Retrieve an unique DueType by its id
+   *
+   * @param   integer $id
+   * @return  DueType
+   */
+  public static function getById($id)
+  {
+    $q = Doctrine_Query::create()
+          ->from('DueType t')
+          ->where('t.id = ?', $id);
+
+    return $q->fetchOne();
+  }
+
+  /**
    * Returns number of existing types for an association
    *
    * @param   integer     $id
@@ -54,5 +69,23 @@ class DueTypeTable extends Doctrine_Table
           ->andWhere('t.association_id = ?', $id);
 
     return $q->execute();
+  }
+
+  /**
+   * Get amount of DueType $id
+   *
+   * @param   integer $id
+   * @return  omteger
+   */
+  public static function getAmountForType($id)
+  {
+    $q = Doctrine_Query::create()
+          ->select('t.amount')
+          ->from('DueType t')
+          ->where('t.id = ?', $id);
+
+    $type = $q->fetchOne();
+
+    return $type->getAmount();
   }
 }
