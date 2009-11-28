6b154075c29b8ec1217605e6f33a1b42c7d6a8e9
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 18:11:43 +0000

    [1.2] DueTypeForm configures fields

--- branches/1.2/lib/form/doctrine/DueTypeForm.class.php
+++ branches/1.2/lib/form/doctrine/DueTypeForm.class.php
@@ -12,5 +12,48 @@ class DueTypeForm extends BaseDueTypeForm
 {
   public function configure()
   {
+    unset($this['created_at'], $this['updated_at']);
+    unset($this['created_by'],  $this['updated_by']);
+    unset($this['state'],       $this['association_id']);
+
+    if ($this->getObject()->isNew())
+    {
+      $this->widgetSchema['created_by'] = new sfWidgetFormInputHidden();
+      $this->widgetSchema['association_id'] = new sfWidgetFormInputHidden();
+      $this->validatorSchema['association_id'] = new sfValidatorInteger();
+      $this->validatorSchema['created_by'] = new sfValidatorInteger();
+    }
+    else
+    {
+      $this->widgetSchema['updated_by'] = new sfWidgetFormInputHidden();
+      $this->validatorSchema['updated_by'] = new sfValidatorInteger();
+    }
+
+    $this->widgetSchema['state'] = new sfWidgetFormInputHidden();
+    $this->setDefault('state', 1);
+    $this->validatorSchema['state'] = new sfValidatorBoolean();
+
+    $this->setDefault('valide', 12);
+    $this->widgetSchema['amount']->setAttribute('class', 'formInputShort');
+    $this->widgetSchema['period']->setAttribute('class', 'formInputShort');
+    $this->widgetSchema['label']->setAttribute('class', 'formInputLarge');
+
+    // We do not allow negative values
+    $this->validatorSchema['amount'] = new sfValidatorNumber(array('min' => 0), array('min' => 'ne peut être négatif'));
+    $this->validatorSchema['period'] = new sfValidatorInteger(array('min' => 0), array('min' => 'ne peut être négatif'));
+
+    $this->setLabels();
+  }
+
+  /**
+   * Set labels for each widgets
+   */
+  protected function setLabels()
+  {
+    $this->widgetSchema->setLabels(array(
+            'amount'  => 'amount',
+            'period'  => 'Valide',
+            'label'   => 'Libellé',
+    ));
   }
 }
