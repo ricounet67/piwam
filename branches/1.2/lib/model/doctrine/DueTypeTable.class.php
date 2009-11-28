c43a2e42d3065a5aa19c1f417ba5f67db31e33ed
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 18:03:13 +0000

    [1.2] DueTypeTable::countForAssociation() has been implemented

--- branches/1.2/lib/model/doctrine/DueTypeTable.class.php
+++ branches/1.2/lib/model/doctrine/DueTypeTable.class.php
@@ -1,6 +1,41 @@
 <?php
-
+/**
+ * Implements operations on DueType table
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @author      Adrien Mogenet
+ * @since       1.2
+ */
 class DueTypeTable extends Doctrine_Table
 {
+  /**
+   * Value of state field when disabled
+   *
+   * @var integer
+   */
+  const STATE_DISABLED    = 0;
 
+  /**
+   * Value of state field when enabled
+   *
+   * @var integer
+   */
+  const STATE_ENABLED     = 1;
+
+  /**
+   * Returns number of existing types for an association
+   *
+   * @param   integer     $id
+   * @return  integer
+   */
+  public static function countForAssociation($id)
+  {
+    $q = Doctrine_Query::create()
+          ->from('DueType t')
+          ->where('t.state = ?', self::STATE_ENABLED)
+          ->andWhere('t.association_id = ?', $id);
+
+    return $q->count();
+  }
 }
