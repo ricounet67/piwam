face41250f2e02402633b4fbec1ab0a99493c1ef
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 23:34:55 +0000

    [1.2] StatusForm::configure() updated_by field is set only when updating

--- branches/1.2/lib/form/doctrine/StatusForm.class.php
+++ branches/1.2/lib/form/doctrine/StatusForm.class.php
@@ -10,7 +10,44 @@
  */
 class StatusForm extends BaseStatusForm
 {
+  /**
+   * Customizes the Statut form. There is a lot of fields to unset in order
+   * to re-create them from scratch with custom behaviour, especially the
+   * hidden references (association, granted user id...)
+   */
   public function configure()
   {
+    $associationId  = sfContext::getInstance()->getUser()->getAttribute('association_id', null, 'user');
+    $userId = sfContext::getInstance()->getUser()->getAttribute('user_id', null, 'user');
+
+    unset($this['created_at'], $this['updated_at']);
+    unset($this['created_by'], $this['updated_by']);
+    unset($this['state'],      $this['association_id']);
+
+    if ($this->getObject()->isNew())
+    {
+      $this->widgetSchema['created_by'] = new sfWidgetFormInputHidden();
+      $this->widgetSchema['association_id'] = new sfWidgetFormInputHidden();
+      $this->setDefault('created_by', $userId);
+      $this->setDefault('association_id', $associationId);
+      $this->validatorSchema['association_id'] = new sfValidatorInteger();
+      $this->validatorSchema['created_by'] = new sfValidatorInteger();
+    }
+    else
+    {
+      $this->widgetSchema['updated_by'] = new sfWidgetFormInputHidden();
+      $this->validatorSchema['updated_by'] = new sfValidatorPass();
+    }
+
+    $this->widgetSchema['state'] = new sfWidgetFormInputHidden();
+    $this->setDefault('state', 1);
+
+    $this->validatorSchema['state'] = new sfValidatorBoolean();
+
+    /**
+     * @todo
+     * FIXME
+     */
+    //$this->validatorSchema['label'] = new sfValidatorCustomUnique(array('class' => 'Statut', 'fields' => array('nom' => null, 'association_id' => $associationId)));
   }
 }
