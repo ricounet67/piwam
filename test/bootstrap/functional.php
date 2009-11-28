00f6cae8f7963281356efa1e9192dbe672e103f9
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-11-28 20:19:34 +0000

    [1.2] Test bootstrapers use Doctrine to load fixtures

--- test/bootstrap/functional.php
+++ test/bootstrap/functional.php
@@ -26,6 +26,6 @@ sfContext::createInstance($configuration);
 sfToolkit::clearDirectory(sfConfig::get('sf_app_cache_dir'));
 
 // load fixtures
-$data = new sfPropelData();
-$data->setDeleteCurrentData(true);
-$data->loadData(sfConfig::get('sf_data_dir') . '/fixtures/test_data.yml');
+//$data = new sfDoctrineDataLoadTask();
+//$data->setDeleteCurrentData(true);
+Doctrine::loadData(sfConfig::get('sf_data_dir') . '/fixtures/test_data.yml');
