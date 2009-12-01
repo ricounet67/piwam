b56447f4875adcb311272c526b91acd81a27ae30
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-01 13:49:57 +0000

    [1.2] depense modules uses new routes

--- branches/1.2/apps/front/modules/depense/templates/indexSuccess.php
+++ branches/1.2/apps/front/modules/depense/templates/indexSuccess.php
@@ -3,9 +3,9 @@
 
 <h2>Gestion des dépenses</h2>
 
-<table class="tableauDonnees">
+<table class="datalist">
     <thead>
-        <tr class="enteteTableauDonnees">
+        <tr>
             <th>Libellé</th>
             <th>Montant</th>
             <th>Compte</th>
@@ -14,35 +14,32 @@
         </tr>
     </thead>
     <tbody>
-    <?php foreach ($depensesPager->getResults() as $depense): ?>
-        <tr>
-            <td><?php echo $depense->getLabel() ?></td>
-            <td><?php echo format_currency($depense->getAmount()) ?></td>
-            <td><?php echo $depense->getAccount() ?></td>
-            <td><?php
-            if ($depense->getPaid() == 1)
-            {
-                echo format_date($depense->getDate());
-            }
-            else
-            {
-                echo 'Non payée';
-            }
-            ?></td>
-            <td><a
-                href="<?php echo url_for('depense/show?id=' . $depense->getId()) ?>"><?php echo image_tag('details.png', array('alt' => '[details]')) ?></a>
-            <a
-                href="<?php echo url_for('depense/edit?id=' . $depense->getId()) ?>"><?php echo image_tag('edit.png', array('alt' => '[modifier]')) ?></a>
-                <?php echo link_to(image_tag('delete', array('alt' => '[supprimer]')),
-          	  					 	'depense/delete?id=' . $depense->getId(),
-                array('method' => 'delete', 'confirm' => 'Ètes vous sûr ?'));
-                ?></td>
-        </tr>
+        <?php foreach ($expensesPager->getResults() as $expense): ?>
+            <tr>
+                <td><?php echo $expense->getLabel() ?></td>
+                <td><?php echo format_currency($expense->getAmount()) ?></td>
+                <td><?php echo $expense->getAccount() ?></td>
+                <td>
+                    <?php if ($expense->getPaid() == 1): ?>
+                        <?php echo format_date($expense->getDate()) ?>
+                    <?php else: ?>
+                        <?php echo 'Non payée'; ?>
+                    <?php endif ?>
+                </td>
+                <td>
+                    <a href="<?php echo url_for('@expense_show?id=' . $expense->getId()) ?>"><?php echo image_tag('details.png', array('alt' => '[details]')) ?></a>
+                    <a href="<?php echo url_for('@expense_edit?id=' . $expense->getId()) ?>"><?php echo image_tag('edit.png', array('alt' => '[modifier]')) ?></a>
+                    <?php echo link_to(image_tag('delete', array('alt' => '[supprimer]')), '@expense_delete?id=' . $expense->getId(), array('method' => 'delete', 'confirm' => 'Ètes vous sûr ?')) ?>
+                </td>
+            </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
 
-        <?php include_partial('global/pager', array('pager' => $depensesPager, 'module' => 'depense', 'action' => 'index', 'params' => array())) ?>
 
-<div class="addNew"><?php echo link_to(image_tag('add', array('align' => 'top', 'alt' => '[ajouter]')). ' Nouvelle dépense', 'depense/new') ?>
+<?php include_partial('global/pager', array('pager' => $expensesPager, 'module' => 'depense', 'action' => 'index', 'params' => array())) ?>
+
+
+<div class="addNew">
+    <?php echo link_to(image_tag('add', array('align' => 'top', 'alt' => '[ajouter]')). ' Nouvelle dépense', '@expense_new') ?>
 </div>
