a4fb9b71f0b774042e885697eded2443c1cb1142
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-01 11:02:07 +0000

    [1.2] New routes are used in association templates

--- branches/1.2/apps/front/modules/association/templates/_configForm.php
+++ branches/1.2/apps/front/modules/association/templates/_configForm.php
@@ -1,22 +1,30 @@
 <?php use_helper('Tooltip') ?>
 
-<form action="<?php echo url_for('association/config') ?>" method="post">
+<form action="<?php echo url_for('@config') ?>" method="post">
 <table class="formArray">
     <tfoot>
         <tr>
             <td>&nbsp;</td>
             <td><input type="submit" class="button" value="Sauvegarder" />
-        
+
         </tr>
     </tfoot>
 
     <?php foreach ($form->getFormFieldSchema() as $widget): ?>
     <tr>
-        <th><?php echo $widget->renderLabel() ?></th>
-        <td><?php echo $widget ?> <?php if ($help = $form->getDescription($widget->getName())): ?>
-        <?php echo tooltip_tag('', $help); ?> <?php endif; ?> <?php echo $widget->renderError() ?>
-        </td>
-        <?php endforeach; ?>
+        <th>
+            <?php echo $widget->renderLabel() ?>
+        </th>
+        <td>
+            <?php echo $widget ?>
 
+            <!-- Display tooltip if description has been provided -->
+            <?php if ($help = $form->getDescription($widget->getName())): ?>
+                <?php echo tooltip_tag('', $help); ?>
+            <?php endif ?>
+
+            <?php echo $widget->renderError() ?>
+        </td>
+        <?php endforeach ?>
 </table>
 </form>
--- branches/1.2/apps/front/modules/association/templates/configSuccess.php
+++ branches/1.2/apps/front/modules/association/templates/configSuccess.php
@@ -1,5 +1,8 @@
 <h2>Configuration</h2>
+
 <?php  if ($sf_user->hasFlash('notice')): ?>
-<p class="notice"><?php echo $sf_user->getFlash('notice') ?></p>
+    <p class="notice"><?php echo $sf_user->getFlash('notice') ?></p>
 <?php endif; ?>
+
+
 <?php include_partial('configForm', array('form' => $form)) ?>
