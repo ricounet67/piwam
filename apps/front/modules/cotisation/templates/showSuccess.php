35690251fbc268ee2dbb895b6778fac5effd8e35
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-01 12:57:27 +0000

    [1.2] New routes for Dues and DueTypes

--- apps/front/modules/cotisation/templates/showSuccess.php
+++ apps/front/modules/cotisation/templates/showSuccess.php
@@ -2,37 +2,37 @@
     <tbody>
         <tr>
             <th>Id:</th>
-            <td><?php echo $cotisation->getId() ?></td>
+            <td><?php echo $due->getId() ?></td>
         </tr>
         <tr>
             <th>Compte:</th>
-            <td><?php echo $cotisation->getCompteId() ?></td>
+            <td><?php echo $due->getAccount() ?></td>
         </tr>
         <tr>
             <th>Cotisation type:</th>
-            <td><?php echo $cotisation->getCotisationTypeId() ?></td>
+            <td><?php echo $due->getDueTypeId() ?></td>
         </tr>
         <tr>
             <th>Membre:</th>
-            <td><?php echo $cotisation->getMembreId() ?></td>
+            <td><?php echo $due->getMember() ?></td>
         </tr>
         <tr>
             <th>Date:</th>
-            <td><?php echo $cotisation->getDate() ?></td>
+            <td><?php echo $due->getDate() ?></td>
         </tr>
         <tr>
             <th>Created at:</th>
-            <td><?php echo $cotisation->getCreatedAt() ?></td>
+            <td><?php echo $due->getCreatedAt() ?></td>
         </tr>
         <tr>
             <th>Updated at:</th>
-            <td><?php echo $cotisation->getUpdatedAt() ?></td>
+            <td><?php echo $due->getUpdatedAt() ?></td>
         </tr>
     </tbody>
 </table>
 
 <hr />
 
-<a href="<?php echo url_for('cotisation/edit?id='.$cotisation->getId()) ?>">Edit</a>
+<a href="<?php echo url_for('cotisation/edit?id='.$due->getId()) ?>">Edit</a>
 &nbsp;
 <a href="<?php echo url_for('cotisation/index') ?>">List</a>
