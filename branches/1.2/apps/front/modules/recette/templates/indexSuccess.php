8d0c901a241b7f56cad2250e08f3b6a7c1e02b94
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-01 16:10:32 +0000

    [1.2] Recette module uses new routes

--- branches/1.2/apps/front/modules/recette/templates/indexSuccess.php
+++ branches/1.2/apps/front/modules/recette/templates/indexSuccess.php
@@ -3,8 +3,8 @@
 
 <h2>Liste des recettes</h2>
 
-<table class="tableauDonnees">
-    <thead class="enteteTableauDonnees">
+<table class="datalist">
+    <thead>
         <tr>
             <th>Libellé</th>
             <th>Montant</th>
@@ -27,20 +27,18 @@
                 echo 'Non perçue';
             }
             ?></td>
-            <td><a
-                href="<?php echo url_for('recette/show?id=' . $recette->getId()) ?>"><?php echo image_tag('details.png', array('alt' => '[détails]')) ?></a>
-            <a
-                href="<?php echo url_for('recette/edit?id=' . $recette->getId()) ?>"><?php echo image_tag('edit.png', array('alt' => '[modifier]')) ?></a>
-                <?php echo link_to(image_tag('delete', array('alt' => '[supprimer]')),
-          	  					 	'recette/delete?id=' . $recette->getId(),
-                array('method' => 'delete', 'confirm' => 'Ètes vous sûr ?'));
-                ?></td>
+            <td>
+                <a href="<?php echo url_for('@income_show?id=' . $recette->getId()) ?>"><?php echo image_tag('details.png', array('alt' => '[détails]')) ?></a>
+                <a href="<?php echo url_for('@income_edit?id=' . $recette->getId()) ?>"><?php echo image_tag('edit.png', array('alt' => '[modifier]')) ?></a>
+                <?php echo link_to(image_tag('delete', array('alt' => '[supprimer]')), '@income_delete?id=' . $recette->getId(), array('method' => 'delete', 'confirm' => 'Ètes vous sûr ?')) ?>
+            </td>
         </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
 
-        <?php include_partial('global/pager', array('pager' => $recettesPager, 'module' => 'recette', 'action' => 'index', 'params' => array())) ?>
+<?php include_partial('global/pager', array('pager' => $recettesPager, 'module' => 'recette', 'action' => 'index', 'params' => array())) ?>
 
-<div class="addNew"><?php echo link_to(image_tag('add', array('align'=>'top', 'alt'=>'[ajouter]')). ' Nouvelle recette', 'recette/new') ?>
+<div class="addNew">
+    <?php echo link_to(image_tag('add', array('align'=>'top', 'alt'=>'[ajouter]')). ' Nouvelle recette', '@income_new') ?>
 </div>
