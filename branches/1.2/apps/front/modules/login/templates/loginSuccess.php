513b913d8679e3e8def0b7b1185dadb835d050dc
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-02 23:18:41 +0000

    [1.2] New login module, with empty default page

---
+++ branches/1.2/apps/front/modules/login/templates/loginSuccess.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="<?php echo $sf_request->getRelativeUrlRoot() ?>/favicon.ico" />
+</head>
+<body>
+
+    <!-- Set Jquery's noConflict mode -->
+
+    <script type="text/javascript">var J = jQuery.noConflict();</script>
+
+    <div id="login">
+        <h1>Piwam</h1>
+        <h2>Authentification</h2>
+    </div>
+</body>
+</html>
