078a9a299738b77538227917297b1c1421301649
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-04 20:12:33 +0000

    [1.2] Expenses are displayed within a partial when display list

--- branches/1.2/apps/front/modules/expense/templates/indexSuccess.php
+++ branches/1.2/apps/front/modules/expense/templates/indexSuccess.php
@@ -10,29 +10,13 @@
             <th>Montant</th>
             <th>Compte</th>
             <th>Date</th>
-            <th>Actions</th>
+            <th class="actions">Actions</th>
         </tr>
     </thead>
     <tbody>
         <?php foreach ($expensesPager->getResults() as $expense): ?>
-            <tr>
-                <td><?php echo $expense->getLabel() ?></td>
-                <td><?php echo format_currency($expense->getAmount()) ?></td>
-                <td><?php echo $expense->getAccount() ?></td>
-                <td>
-                    <?php if ($expense->getPaid() == 1): ?>
-                        <?php echo format_date($expense->getDate()) ?>
-                    <?php else: ?>
-                        <?php echo 'Non payée'; ?>
-                    <?php endif ?>
-                </td>
-                <td>
-                    <a href="<?php echo url_for('@expense_show?id=' . $expense->getId()) ?>"><?php echo image_tag('details.png', array('alt' => '[details]')) ?></a>
-                    <a href="<?php echo url_for('@expense_edit?id=' . $expense->getId()) ?>"><?php echo image_tag('edit.png', array('alt' => '[modifier]')) ?></a>
-                    <?php echo link_to(image_tag('delete', array('alt' => '[supprimer]')), '@expense_delete?id=' . $expense->getId(), array('method' => 'delete', 'confirm' => 'Ètes vous sûr ?')) ?>
-                </td>
-            </tr>
-        <?php endforeach; ?>
+            <?php include_partial('expenseRow', array('expense' => $expense)) ?>
+        <?php endforeach ?>
     </tbody>
 </table>
 
