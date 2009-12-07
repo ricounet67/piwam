f72c71bcd0ff26f8b8dc41d1a76feb68ec372fd0
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-07 23:31:38 +0000

    [trunk] Merged with last revision of 1.2 branch. Fixed wrong route in _accountRow

--- apps/front/modules/account/templates/_accountRow.php
+++ apps/front/modules/account/templates/_accountRow.php
@@ -10,8 +10,8 @@
     <td><?php echo $account->getReference() ?></td>
     <td><?php echo format_date($account->getCreatedAt()) ?></td>
     <td>
-      <a href="<?php echo url_for('@account_by_id?id='.$account->getId()) ?>"><?php echo image_tag('icons/show.png', array('alt' => '[details]')); ?></a>
-      <a href="<?php echo url_for('@account_edit?id='.$account->getId()) ?>"><?php echo image_tag('icons/edit.png', array('alt' => '[modifier]')); ?></a>
-      <?php echo link_to(image_tag('icons/delete', array('alt' => '[supprimer]')), '@account_delete?id=' . $account->getId(), array('method' => 'delete', 'confirm' => 'Ètes vous sûr ?')); ?>
+      <?php echo link_to(image_tag('icons/show', array('alt' => '[details]')),  '@account_show?id='.$account->getId()) ?>
+      <?php echo link_to(image_tag('icons/edit', array('alt' => '[modifier]')), '@account_edit?id='.$account->getId()) ?>
+      <?php include_partial('global/deleteButton', array('route' => '@account_delete', 'id' => $account->getId())) ?>
     </td>
 </tr>
