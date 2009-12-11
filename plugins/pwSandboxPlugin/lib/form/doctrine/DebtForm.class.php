194c15fc37b70858caa0365060dbe6a57a8d633f
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-11 19:06:44 +0000

    [1.2] pwSandboxPlugin : DebtForm now requires an association ID

--- plugins/pwSandboxPlugin/lib/form/doctrine/DebtForm.class.php
+++ plugins/pwSandboxPlugin/lib/form/doctrine/DebtForm.class.php
@@ -19,6 +19,16 @@ class DebtForm extends PluginDebtForm
   public function configure()
   {
     /*
+     * You will see that we will need the association ID of the current
+     * user. As you saw in actions.class.php, we gave the ID as parameter
+     * to this form ; so, let's save it in $id variable !
+     */
+    if (! $id = $this->getOption('associationId'))
+    {
+      throw new InvalidArgumentException('You must provide an ID');
+    }
+
+    /*
      * We can specify explicitely which fields we want to use.
      * That also defines the order fields will appear.
      *
@@ -62,7 +72,18 @@ class DebtForm extends PluginDebtForm
      * The last line uncheck the 'received' checkbox
      */
 
-    
+    /*
+     * Hop ! Don't move on, it's not over ! Currently, the list of members
+     * displays ALL the members. But if Piwam is managing several associations
+     * at the same time, we want to display members who belong only to the
+     * current user's association. The $id variable is finally useful !
+     *
+     * We will customize the default `member_id` field by giving a
+     * Doctrine_Query object. We use a method already implemented in the
+     * Piwam's core :
+     */
+    $this->widgetSchema['member_id']->setOption('query', MemberTable::getQueryEnabledForAssociation($id));
+
     /*
      * Finally, this is preferable to set labels of each field (at least
      * to display it in French ;-)
