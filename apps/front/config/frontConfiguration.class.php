da034459051f5f05e7b4470f894f3889cd837c2b
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-11 09:35:18 +0000

    [1.2] Removed useless constants in frontConfiguration.class.php

--- apps/front/config/frontConfiguration.class.php
+++ apps/front/config/frontConfiguration.class.php
@@ -13,13 +13,6 @@ class frontConfiguration extends sfApplicationConfiguration
    */
 	public function configure()
 	{
-		// Represents the states of `actif` fields
-		//define('ENABLED',		1);
-		//define('DISABLED',  0);
-
-		// Symbol which is used as CSV separator when exporting files
-		//define('CSV_SEPARATOR',	',');
-
 		// Define error messages for required/invalid fields
 		sfValidatorBase::setDefaultMessage('required', 'Requis');
 		sfValidatorBase::setDefaultMessage('invalid', 'Invalide');
