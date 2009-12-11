8d613d4fcf28dfed724878f2e4d971bab174a267
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-11 11:10:12 +0000

    [1.2] Initial tree of pwSandboxPlugin

---
+++ plugins/pwSandboxPlugin/modules/pwSandbox/actions/actions.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * New actions provided by the pwSandboxPlugin
+ *
+ * @package     pwSandboxPlugin
+ * @subpackage  actions
+ * @author      adrien
+ * @since       1.2
+ */
+class pwSandboxActions extends sfActions
+{
+  /**
+   * Index action
+   */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
+?>
