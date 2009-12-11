722f3b057f9668a67963e58da4368e722df6bfc3
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-11 16:39:29 +0000

    [1.2] Default value of updated_by field is set directly in IncomeForm

--- lib/form/doctrine/IncomeForm.class.php
+++ lib/form/doctrine/IncomeForm.class.php
@@ -31,6 +31,7 @@ class IncomeForm extends BaseIncomeForm
     }
 
     $this->widgetSchema['updated_by'] = new sfWidgetFormInputHidden();
+    $this->setDefault('updated_by', sfContext::getInstance()->getUser()->getUserId());
     $this->validatorSchema['updated_by'] = new sfValidatorInteger();
     $this->validatorSchema['amount'] = new sfValidatorAmount(array('min' => 0), array('min' => 'ne peut être négatif'));
 
