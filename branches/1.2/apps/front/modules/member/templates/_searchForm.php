a13c37a94038be485ce6fee88b404bac56fc453a
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-12 12:51:06 +0000

    [1.2] Removed div from member/_searchForm partial

--- branches/1.2/apps/front/modules/member/templates/_searchForm.php
+++ branches/1.2/apps/front/modules/member/templates/_searchForm.php
@@ -7,10 +7,7 @@
 <?php use_javascript('/sfFormExtraPlugin/js/jquery.autocompleter.js') ?>
 <?php use_stylesheet('/sfFormExtraPlugin/css/jquery.autocompleter.css') ?>
 
-
-<div id="searchBar">
-    <form action="<?php echo url_for('@member_search'); ?>" method="post">
-        <?php echo $form->renderHiddenFields() ?>
-        <?php echo $form['magic'] ?> <input type="submit" name="submit" value="rechercher" class="small blue button" />
-    </form>
-</div>
+<form action="<?php echo url_for('@member_search'); ?>" method="post">
+    <?php echo $form->renderHiddenFields() ?>
+    <?php echo $form['magic'] ?> <input type="submit" name="submit" value="rechercher" class="small blue button" />
+</form>
