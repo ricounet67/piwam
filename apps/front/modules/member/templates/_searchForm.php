ebde4e38afedc34c198fe282a650ccbcc51b1e07
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-12 13:21:28 +0000

    [1.2] Removed _filterForm partial

--- apps/front/modules/member/templates/_searchForm.php
+++ apps/front/modules/member/templates/_searchForm.php
@@ -8,7 +8,13 @@
 <?php use_stylesheet('/sfFormExtraPlugin/css/jquery.autocompleter.css') ?>
 
 <form action="<?php echo url_for('@member_search'); ?>" method="post">
-    Prénom / Nom :
-    <?php echo $form->renderHiddenFields() ?>
-    <?php echo $form['magic'] ?> <input type="submit" name="submit" value="rechercher" class="small blue button" />
+  <?php echo $form['due_state']->renderLabel() ?>
+  <?php echo $form['due_state']->render() ?>
+
+  <?php echo $form['by_page']->renderLabel() ?>
+  <?php echo $form['by_page']->render() ?>
+
+  Prénom / Nom :
+  <?php echo $form->renderHiddenFields() ?>
+  <?php echo $form['magic'] ?> <input type="submit" name="submit" value="rechercher" class="small blue button" />
 </form>
