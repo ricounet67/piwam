00174e4705169932f7bc6a73fd13e61303747d81
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2009-12-19 16:36:17 +0000

    [1.2] Commented class "CheckDBAvailabilityFilter"

--- branches/1.2/apps/front/lib/filter/CheckDBAvailabilityFilter.class.php
+++ branches/1.2/apps/front/lib/filter/CheckDBAvailabilityFilter.class.php
@@ -1,16 +1,43 @@
 <?php
+/**
+ * Checks the availability of the working database, and redirects
+ * to a module if it is not reachable.
+ *
+ * @since 1.2
+ * @see   http://www.funstaff.ch
+ */
 class CheckDBAvailabilityFilter extends sfFilter
 {
-  public function execute($filterChain)
-  {
-    $module = 'install';
-    $action = 'index';
+  /**
+   * The destination module that will be reached if database
+   * is not available
+   *
+   * @var string
+   */
+  const MODULE = 'install';
 
+  /**
+   * The destination action that will be called if database
+   * is not reachable
+   *
+   * @var string
+   */
+  const ACTION = 'index';
+
+  /**
+   * Checks availability of databases according to the Doctrine
+   * connection's names defined in configuration files
+   *
+   * @param   sfFilterChain  $filterChain
+   * @return  sfFilterChain
+   */
+  public function execute(sfFilterChain $filterChain)
+  {
     if ($this->isFirstCall())
     {
       $context = $this->getContext();
 
-      if (($module != $context->getModuleName()))
+      if ((self::MODULE != $context->getModuleName()))
       {
         $configuration = sfProjectConfiguration::getActive();
         $db = new sfDatabaseManager($configuration);
@@ -23,7 +50,8 @@ class CheckDBAvailabilityFilter extends sfFilter
           }
           catch(Exception $e)
           {
-             $context->getController()->forward($module, $action);
+             $context->getController()->forward(self::MODULE, self::ACTION);
+             
              exit;
           }
         }
