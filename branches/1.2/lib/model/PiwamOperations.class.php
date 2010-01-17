977d0ff878b03bc25861c07c86e5edc1ee4680e8
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-01-17 22:11:22 +0000

    [1.2] New class PiwamOperations

---
+++ branches/1.2/lib/model/PiwamOperations.class.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file is part of the piwam package.
+ * (c) Adrien Mogenet <adrien.mogenet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Operations which deal with other Model files
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @author      Adrien
+ * @since       1.2
+ */
+class PiwamOperations
+{
+
+  /**
+   * Checks if Piwam is alreay installed. It's currently based on SQL
+   * configuration, and check if piwam tables are present or not. This method
+   * has been set as static because other foregin methods may want to check
+   * if Piwam has been properly installed (ie: default module/action)
+   *
+   * @return boolean
+   */
+  public static function isInstalled()
+  {
+    try
+    {
+      Doctrine_Manager::connection();
+      AssociationTable::doCount();
+      ConfigVariableTable::doCount();
+      MemberTable::doCount();
+      StatusTable::doCount();
+      AccountTable::doCount();
+      DueTable::doCount();
+      DueTypeTable::doCount();
+      IncomeTable::doCount();
+      ExpenseTable::doCount();
+      assert(AclActionTable::doCount() > 10);
+      assert(ConfigVariableTable::doCount() > 10);
+
+      return true;
+    }
+    catch (Doctrine_Connection_Exception $e)
+    {
+      return false;
+    }
+  }
+}
+?>
