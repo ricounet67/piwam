4058d4eb37ea4d6886e0fec02f7c999be307c835
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-02-19 22:30:14 +0000

    [1.2] MemberExtraRowTable::getForAssociation() implemented

--- branches/1.2/lib/model/doctrine/MemberExtraRowTable.class.php
+++ branches/1.2/lib/model/doctrine/MemberExtraRowTable.class.php
@@ -1,5 +1,29 @@
 <?php
-
+/**
+ * Implement operations on MemberExtraRow table. This table
+ * stores extra rows that we would like to add to member
+ * definition.
+ *
+ * @package     piwam
+ * @subpackage  model
+ * @author      Adrien Mogenet
+ * @since       1.2
+ */
 class MemberExtraRowTable extends Doctrine_Table
 {
+  /**
+   * Retrieve rows for association $id
+   *
+   * @param   integer         $id
+   * @return  array of MemberExtraRow
+   */
+  public static function getForAssociation($id)
+  {
+    $q = Doctrine_Query::create()
+          ->from('MemberExtraRow r')
+          ->where('r.association_id = ?', $id)
+          ->orderBy('r.id DESC');
+
+    return $q->execute();
+  }
 }
