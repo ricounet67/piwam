4301dd71b4bdf5aa46864a8bfc7874f3a7d24d20
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-02-20 13:44:40 +0000

    [1.2] Fixed syntax error in MemberExtraRow, and new methods getParameterAsXXX

--- lib/model/doctrine/MemberExtraRow.class.php
+++ lib/model/doctrine/MemberExtraRow.class.php
@@ -14,12 +14,59 @@ class MemberExtraRow extends BaseMemberExtraRow
   /**
    * Parse the 'type' field in Database and return the real
    * type. (ie: `string 123 => string`)
+   *
+   * @return  string
    */
   public function getType()
   {
-    $original = $this->__get('type');
+    $original = $this->_get('type');
     $split = explode(' ', $original);
 
     return $split[0];
+  }
+
+  /**
+   * Return list of parameters as a list of choices. Allows to
+   * check errors if any
+   *
+   * @return  arrau
+   */
+  public function getParametersAsChoices()
+  {
+    $original = $this->_get('type');
+    $list = substr($original, 8);
+
+    return explode(',', $list);
+  }
+
+  /**
+   * Return the parameter as an integer. Allows to
+   * check errors if any
+   */
+  public function getParameterAsInt()
+  {
+    $original = $this->_get('type');
+    $split = explode(' ', $original);
+
+    if ((count($split) === 2) && (ctype_digit($split[1])))
+    {
+      return $split[1];
+    }
+    else
+    {
+      throw new sfException('Error when parsing type of extra
+        row "' . $this->getLabel() . '"');
+    }
+  }
+
+  /**
+   * Automatically slugify the label
+   *
+   * @param   string  $value
+   * @return  string  The slugified label
+   */
+  public function setLabel($value)
+  {
+    $this->_set('label', $value);
   }
 }
