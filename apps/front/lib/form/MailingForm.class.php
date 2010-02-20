bed8c930569a327a122bdea5e3fd851113775dc6
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-02-20 11:57:48 +0000

    [1.2] Well re-formatted MailingForm

--- apps/front/lib/form/MailingForm.class.php
+++ apps/front/lib/form/MailingForm.class.php
@@ -13,20 +13,20 @@ class MailingForm extends BaseForm
   public function configure()
   {
     $this->setWidgets(array(
-      'subject'		=> new sfWidgetFormInputText(),
+      'subject'       => new sfWidgetFormInputText(),
       'mail_content' 	=> new sfWidgetFormTextareaTinyMCE(array(
-        'width'  => 550,
-        'height' => 350,
-        'config' => '	theme_advanced_buttons1 : "bold,italic,underline,fontsizeselect,fontselect,forecolorpicker,image,link,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,indent,outdent",
-                      theme_advanced_buttons2 : "",
-                      theme_advanced_buttons3 : "",
-                      theme_advanced_statusbar_location : "none"'
+        'width'       => 550,
+        'height'      => 350,
+        'config'      => '	theme_advanced_buttons1 : "bold,italic,underline,fontsizeselect,fontselect,forecolorpicker,image,link,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,indent,outdent",
+                            theme_advanced_buttons2 : "",
+                            theme_advanced_buttons3 : "",
+                            theme_advanced_statusbar_location : "none"'
         ),
       array('rows' => 40, 'cols' => 10)),
     ));
 
     $this->setValidators(array(
-      'subject'		=> new sfValidatorString(array('required' => true)),
+      'subject'       => new sfValidatorString(array('required' => true)),
       'mail_content'	=> new sfValidatorString(array('required' => true)),
     ));
 
