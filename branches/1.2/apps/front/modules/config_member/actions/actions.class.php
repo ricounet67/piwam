a053740343d45975da93c240bc7961d094ebae49
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-02-20 11:47:55 +0000

    [1.2] config_memberActions processes the extra 'parameter' field

--- branches/1.2/apps/front/modules/config_member/actions/actions.class.php
+++ branches/1.2/apps/front/modules/config_member/actions/actions.class.php
@@ -9,38 +9,46 @@
  */
 class config_memberActions extends sfActions
 {
- /**
-  * Main screen to customize extra rows. Process form if it has been
-  * submit.
-  *
-  * @param sfRequest $request A request object
-  */
+  /**
+   * Main screen to customize extra rows. Process form if it has been
+   * submit.
+   *
+   * @param sfRequest $request A request object
+   */
   public function executeIndex(sfWebRequest $request)
   {
     $associationId = $this->getUser()->getAssociationId();
     $this->form = new MemberExtraRowForm();
     $this->form->setDefault('association_id', $this->getUser()->getAssociationId());
+    $this->form->setDefault('parameter', '');
 
     if ($request->isMethod('post'))
     {
       $this->_processForm($request, $this->form);
     }
-    
+
     $this->extraRows = MemberExtraRowTable::getForAssociation($associationId);
   }
 
   /*
-   * Process form values
+   * Process form values. Concatenate selected type of the new field and
+   * the related parameters if required
    *
    * @param MemberExtraRowForm $form
-   */
+  */
   private function _processForm(sfWebRequest $request, MemberExtraRowForm $form)
   {
     $form->bind($request->getParameter($form->getName()));
 
     if ($form->isValid())
     {
+      $data = $request->getParameter('member_extra_row');
+      $data['type'] .= ' ' . $data['parameter'];
+
       $row = $form->save();
+      $row->setType($data['type']);
+      $row->save();
+
       $this->getUser()->setFlash('notice', 'Champ ajouté avec succès.');
     }
   }
