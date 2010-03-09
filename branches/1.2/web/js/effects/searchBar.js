1a940d7cd863bc3537b3f8514ce48139344c8139
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-03-09 20:55:01 +0000

    [1.2] The search bar is hidden by default

--- branches/1.2/web/js/effects/searchBar.js
+++ branches/1.2/web/js/effects/searchBar.js
@@ -1,8 +1,11 @@
 /* 
- * Show and hide the search bar
+ * Show and hide the search bar.
+ *
+ * Default state : hidden bar
  */
 
 $(document).ready(function() {
+  $('#searchBar').slideUp(0);
   $('a#toggle-searchbar').click(function() {
     $('#searchBar').slideToggle(400);
     return false;
