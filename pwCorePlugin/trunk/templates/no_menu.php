45cb6d80f436a05c917ecadcac184df12decadec
nicolas.charlot <nicolas.charlot@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-03-24 10:38:39 +0000

    [pwCorePlugin / trunk] Included default JS and CSS in default layouts

--- pwCorePlugin/trunk/templates/no_menu.php
+++ pwCorePlugin/trunk/templates/no_menu.php
@@ -1,11 +1,24 @@
+<?php use_javascript('/pwCorePlugin/js/boxover/boxover.js') ?>
+<?php use_javascript('/pwCorePlugin/js/jquery-ui-1.7.1/js/jquery-1.3.2.min.js') ?>
+<?php use_javascript('/pwCorePlugin/js/jquery-ui-1.7.1/js/jquery-ui-1.7.2.custom.min.js') ?>
+<?php use_javascript('/pwCorePlugin/js/jquery-tools/jquery.tools.min.js') ?>
+<?php use_stylesheet('/pwCorePlugin/css/buttons.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/form.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/main.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/menu.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/overlay.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/pagination.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/table.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/jquery-ui-1.7.1/smoothness/jquery-ui-1.7.1.custom.css') ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
+    <?php include_stylesheets() ?>        
     <?php include_javascripts() ?>
-    <link rel="shortcut icon" href="<?php echo $sf_request->getRelativeUrlRoot() ?>/favicon.ico" />
+    <link rel="shortcut icon" href="<?php echo $sf_request->getRelativeUrlRoot() ?>/pwCorePlugin/images/favicon.ico" />
 
     <style type="text/css">
       div#centeredContent {
