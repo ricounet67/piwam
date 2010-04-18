d65d507f438b3af1a4bbe309801415286f2fe007
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-04-18 18:17:43 +0000

    [pwCorePlugin / trunk] Fixed CSS path in member/edit  template

--- pwCorePlugin/trunk/modules/member/templates/editSuccess.php
+++ pwCorePlugin/trunk/modules/member/templates/editSuccess.php
@@ -1,30 +1,31 @@
 <?php use_javascript('/pwCorePlugin/js/domtab/domtab.js') ?>
-<?php use_stylesheet('/pwCorePlugin/js/domtab.css') ?>
+<?php use_stylesheet('/pwCorePlugin/css/domtab.css') ?>
 
 <div class="domtab">
 
-    <!-- List of tabs -->
-    <ul class="domtabs">
-        <li><a href="#profil">Profil du membre</a></li>
-        <li><a href="#credentials">Gestion des droits</a></li>
-    </ul>
+  <!-- List of tabs -->
+  <ul class="domtabs">
+    <li><a href="#profil">Profil du membre</a></li>
+    <li><a href="#credentials">Gestion des droits</a></li>
+  </ul>
 
-    <!-- First tab : edit profile -->
-    <div>
-        <h2><a name="profil" id="profil">Editer les informations</a></h2>
-        <?php include_partial('form', array('form' => $form)) ?>
-    </div>
+  <!-- First tab : edit profile -->
+  <div>
+    <h2><a name="profil" id="profil">Editer les informations</a></h2>
+    <?php include_partial('form', array('form' => $form)) ?>
+  </div>
 
-    <!-- Second tab : edit credentials -->
-    <div>
-        <h2><a name="credentials" id="credentials">Gérer les droits</a></h2>
-        <?php if ($canEditRight): ?>
-            <?php include_partial('aclForm', array('form' => $aclForm, 'user_id' => $user_id)) ?>
-        <?php else: ?>
-            <p>
-                Désolé, vous n'avez pas les droits suffisants pour éditer les droits de
-                l'utilisateur !
-            </p>
-        <?php endif; ?>
-    </div>
+  <!-- Second tab : edit credentials -->
+  <div>
+    <h2><a name="credentials" id="credentials">Gérer les droits</a></h2>
+    <?php if ($canEditRight): ?>
+      <?php include_partial('aclForm', array('form' => $aclForm, 'user_id' => $user_id)) ?>
+    <?php else: ?>
+      <p>
+        Désolé, vous n'avez pas les droits suffisants pour éditer les droits de
+        l'utilisateur !
+      </p>
+    <?php endif ?>
+  </div>
+  
 </div>
