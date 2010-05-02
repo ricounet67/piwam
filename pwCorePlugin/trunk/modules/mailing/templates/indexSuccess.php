78d908e7272dbdf58f59d4bbc6f50812c1f41ead
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-02 20:19:15 +0000

    [pwCorePlugin / trunk] Enhanced mailing/index template

--- pwCorePlugin/trunk/modules/mailing/templates/indexSuccess.php
+++ pwCorePlugin/trunk/modules/mailing/templates/indexSuccess.php
@@ -5,14 +5,15 @@
 <!-- Display success or error message -->
 
 <?php if ($sf_user->hasFlash('notice')): ?>
-    <p class="notice"><?php echo $sf_user->getFlash('notice') ?></p>
+  <p class="notice"><?php echo $sf_user->getFlash('notice') ?></p>
 <?php endif ?>
 
 <?php if ($sf_user->hasFlash('error')): ?>
-    <p class="error">
-        <?php echo image_tag('/pwCorePlugin/images/error', array('align' => 'top', 'alt' => 'Erreur')) ?> <strong>ERREUR</strong>:
-        <?php echo $sf_user->getFlash('error') ?>
-    </p>
+  <p class="error">
+    <?php echo image_tag('/pwCorePlugin/images/error', array('align' => 'top', 'alt' => 'Erreur')) ?>
+    <strong>ERREUR</strong>:
+    <?php echo $sf_user->getFlash('error') ?>
+  </p>
 <?php endif ?>
 
 
@@ -20,47 +21,47 @@
 
 <?php  if (isset($content)): ?>
 
-    <div class="mailPreview">
-        <p><strong>Votre message :</strong></p>
-        <hr />
-        <?php echo html_entity_decode($content) ?>
-    </div>
+  <div class="mailPreview">
+    <p><strong>Votre message :</strong></p>
+    <hr />
+    <?php echo html_entity_decode($content) ?>
+  </div>
 
 <?php else: ?>
 
-    <form action="<?php echo url_for('mailing/index') ?>" method="post">
+  <form action="<?php echo url_for('mailing/index') ?>" method="post">
+  <?php echo $form->renderHiddenFields() ?>
+
+    <!-- if message or subject is empty -->
+
+    <?php if ($form->hasErrors()): ?>
+      <div class="global_errors">
+        <?php echo $form->renderGlobalErrors() ?>
+      </div>
+    <?php endif ?>
+
+
     <table class="formtable" title="mailing">
-
-        <!-- if message or subject is empty -->
-
-        <?php if ($form->hasErrors()): ?>
-            <tr>
-                <td colspan="2">
-                    <div class="error">
-                        <?php echo image_tag('/pwCorePlugin/images/error', array('alt' => '[Erreur]', 'align' => 'top')) ?>
-                        ERREUR : Vous devez entrer un sujet et un message
-                    </div>
-                </td>
-            </tr>
-        <?php endif ?>
-
-        <tr>
-            <th><?php echo $form['subject']->renderLabel() ?></th>
-            <td><?php echo $form['subject'] ?></td>
-        </tr>
-        <tr>
-            <th>Destinataires</th>
-            <td>Tout le monde</td>
-        </tr>
-        <tr>
-            <th valign="top"><?php echo $form['mail_content']->renderLabel() ?></th>
-            <td><?php echo $form['mail_content'] ?></td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td><input type="submit" class="grey button" value="Envoyer" /></td>
-        </tr>
+      <tr>
+        <th><?php echo $form['subject']->renderLabel() ?></th>
+        <td><?php echo $form['subject'] ?></td>
+      </tr>
+      <tr>
+        <th>Destinataires</th>
+        <td>Tout le monde</td>
+      </tr>
+      <tr>
+        <th valign="top"><?php echo $form['mail_content']->renderLabel() ?></th>
+        <td><?php echo $form['mail_content'] ?></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>
+          <input type="submit" class="grey button" value="Envoyer" />
+          <?php echo link_to('Liste des emails envoyés', '@mailing_list', array('class' => 'blue button')) ?>
+        </td>
+      </tr>
     </table>
-    </form>
+  </form>
 
 <?php endif ?>
