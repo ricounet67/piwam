67e3c5218aa535a9977237a3ec39ec549cd37633
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-02 08:58:11 +0000

    [pwCorePlugin / trunk] Reindented MailerFactory

--- pwCorePlugin/trunk/lib/misc/MailerFactory.class.php
+++ pwCorePlugin/trunk/lib/misc/MailerFactory.class.php
@@ -7,56 +7,56 @@
  */
 class MailerFactory
 {
-    /**
-     * Build a swift mailer object according to the configuration.
-     * The user is able to select the method he prefers to use
-     * for sending emails. By default we use the mail() php function
-     *
-     * @param   integer                 $associationId
-     * @param   sfUser                  $sfUser
-     * @return  Swift_Mailer
-     */
-    public static function get($associationId, $sfUser = null)
+  /**
+   * Build a swift mailer object according to the configuration.
+   * The user is able to select the method he prefers to use
+   * for sending emails. By default we use the mail() php function
+   *
+   * @param   integer                 $associationId
+   * @param   sfUser                  $sfUser
+   * @return  Swift_Mailer
+   */
+  public static function get($associationId, $sfUser = null)
+  {
+    switch (Configurator::get('method', $associationId, 'mail'))
     {
-        switch (Configurator::get('method', $associationId, 'mail'))
+      case 'gmail': // yes this is just a special case for smtp ;-)
+        $methodObject = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'tls');
+        $methodObject->setUsername(Configurator::get('gmail_username', $associationId));
+        $methodObject->setPassword(Configurator::get('gmail_password', $associationId));
+
+        if (!extension_loaded('openssl') && $sfUser)
         {
-            case 'gmail': // yes this is just a special case for smtp ;-)
-                $methodObject = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'tls');
-                $methodObject->setUsername(Configurator::get('gmail_username', $associationId));
-                $methodObject->setPassword(Configurator::get('gmail_password', $associationId));
-
-                if (!extension_loaded('openssl') && $sfUser)
-                {
-                    $sfUser->setFlash('error', 'Le module "openssl" n\'est pas activé. Veuillez l\'activer ou changer la méthode d\'envoi d\' e-mails');
-                }
-                break;
-
-            case 'smtp':
-                $smtpServer = Configurator::get('smtp_server', $associationId);
-                $smtpPort = null;
-                $smtpEncryption = null;
-                $smtpUsername = Configurator::get('smtp_username', $associationId);
-                $smtpPassword = Configurator::get('smtp_password', $associationId);
-                $methodObject = Swift_SmtpTransport::newInstance($smtpServer, $smtpPort, $smtpEncryption);
-                $methodObject->setUsername($smtpUsername);
-                $methodObject->setPassword($smtpPassword);
-                break;
-
-            case 'sendmail':
-                $sendmailPath = Configurator::get('sendmail_path', $associationId, '/usr/bin/sendmail');
-                $methodObject = Swift_SendmailTransport::newInstance($sendmailPath . ' -bs');
-                break;
-
-            case 'mail':
-                $methodObject = new Swift_MailTransport();
-                break;
-
-            default:
-                $methodObject = new Swift_MailTransport();
-                break;
+          $sfUser->setFlash('error', 'Le module "openssl" n\'est pas activé. Veuillez l\'activer ou changer la méthode d\'envoi d\' e-mails');
         }
+        break;
 
-        return Swift_Mailer::newInstance($methodObject);
+      case 'smtp':
+        $smtpServer = Configurator::get('smtp_server', $associationId);
+        $smtpPort = null;
+        $smtpEncryption = null;
+        $smtpUsername = Configurator::get('smtp_username', $associationId);
+        $smtpPassword = Configurator::get('smtp_password', $associationId);
+        $methodObject = Swift_SmtpTransport::newInstance($smtpServer, $smtpPort, $smtpEncryption);
+        $methodObject->setUsername($smtpUsername);
+        $methodObject->setPassword($smtpPassword);
+        break;
+
+      case 'sendmail':
+        $sendmailPath = Configurator::get('sendmail_path', $associationId, '/usr/bin/sendmail');
+        $methodObject = Swift_SendmailTransport::newInstance($sendmailPath . ' -bs');
+        break;
+
+      case 'mail':
+        $methodObject = new Swift_MailTransport();
+        break;
+
+      default:
+        $methodObject = new Swift_MailTransport();
+        break;
     }
+
+    return Swift_Mailer::newInstance($methodObject);
+  }
 }
 ?>
