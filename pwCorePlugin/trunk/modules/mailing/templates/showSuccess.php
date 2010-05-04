de978db25b4d7f1ba767b55d09fe036727847417
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-04 19:49:06 +0000

    [pwCorePlugin / trunk] Designed mailing/show template

--- pwCorePlugin/trunk/modules/mailing/templates/showSuccess.php
+++ pwCorePlugin/trunk/modules/mailing/templates/showSuccess.php
@@ -8,3 +8,32 @@
  */
 ?>
 <h2>E-mail envoyé</h2>
+
+<table class="details">
+  <tbody>
+    <tr>
+      <th>Envoyé le</th>
+      <td><?php echo $email->getCreatedAt() ?> par <?php echo $email->getSentByMember() ?></td>
+    </tr>
+    <tr>
+      <th>Destinataires</th>
+      <td><?php echo implode(',', $email->getAddresses()) ?></td>
+    </tr>
+    <tr>
+      <th>Sujet</th>
+      <td><?php echo $email->getObject() ?></td>
+    </tr>
+    <tr>
+      <th>Contenu</th>
+      <td><?php echo $email->getMessage() ?></td>
+    </tr>
+    <tr>
+      <th>Nombre d'envois avec succès</th>
+      <td><?php echo $email->getSuccess() ?></td>
+    </tr>
+    <tr>
+      <th>Nombre d'erreurs</th>
+      <td><?php echo $email->getErrors() ?></td>
+    </tr>
+  </tbody>
+</table>
