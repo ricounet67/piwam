d7355b207b0af83b3facd15edaee3586ae05dfa3
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-04 19:34:17 +0000

    [pwCorePlugin / trunk] Designed mailing/list template

--- pwCorePlugin/trunk/modules/mailing/templates/listSuccess.php
+++ pwCorePlugin/trunk/modules/mailing/templates/listSuccess.php
@@ -6,5 +6,28 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+use_helper('Date');
 ?>
 <h2>E-mails envoyés</h2>
+
+<table class="datalist" summary="Sent emails">
+  <thead>
+    <tr>
+      <th width="130px">Date d'envoi</th>
+      <th>Sujet</th>
+      <th width="60px">Succès</th>
+      <th width="60px">Erreurs</th>
+      <th width="100px">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <?php foreach ($emails->getResults() as $email): ?>
+      <?php include_partial('emailRow', array('email' => $email)) ?>
+    <?php endforeach ?>
+
+  </tbody>
+</table>
+
+<?php include_partial('global/pager', array('pager' => $emails, 'route' => '@mailing_list', 'params' => array())) ?>
