8dd20c304cd757c432d27f9fe1731901bace8e6d
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-04 19:34:54 +0000

    [pwCorePlugin / trunk] Created mailing/_emailRow partial to display a sent email

---
+++ pwCorePlugin/trunk/modules/mailing/templates/_emailRow.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the piwam package.
+ * (c) Adrien Mogenet <adrien.mogenet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * Input : SentMail $email
+ */
+?>
+<tr>
+  <td><?php echo format_datetime($email->getCreatedAt(), 'dd/MM/yyyy à HH:mm') ?></td>
+  <td><?php echo $email->getObject() ?></td>
+  <td class="numbers"><?php echo $email->getSuccess() ?></td>
+  <td class="numbers"><?php echo $email->getErrors() ?></td>
+  <td>X</td>
+</tr>
