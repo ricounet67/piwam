ea374017234b014ee2aa0885d17cbef637099557
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-11 20:19:11 +0000

    [pwCorePlugin / trunk] Added new module bookkeeping

---
+++ pwCorePlugin/trunk/modules/bookkeeping/lib/BaseBookkeepingActions.class.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the piwam package.
+ * (c) Adrien Mogenet <adrien.mogenet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+/**
+ * Bookkeeping actions.
+ *
+ * @package    piwam
+ * @subpackage association
+ * @author     Adrien Mogenet
+ * @version    SVN: $Id: actions.class.php 12474 2010-05-11 10:41:27Z adrien $
+ * @since      1.2
+ */
+class BaseBookkeepingActions extends sfActions
+{
+  /**
+   * Home of bookkeeping module
+   *
+   * @param sfWebRequest $request
+   */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
+?>
