affda3b098b8f26117f99f0894fd3081848cafb2
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-18 19:24:53 +0000

    [pwCorePlugin / trunk] Implemented basis for EntryForm

--- pwCorePlugin/trunk/lib/form/doctrine/PluginEntryForm.class.php
+++ pwCorePlugin/trunk/lib/form/doctrine/PluginEntryForm.class.php
@@ -10,4 +10,49 @@
  */
 abstract class PluginEntryForm extends BaseEntryForm
 {
+  /**
+   * Setup form widgets and behaviour
+   */
+  public function setup()
+  {
+    parent::setup();
+    $this->useFields(array('date', 'label'));
+
+    if (! $user = $this->getOption('user'))
+    {
+      throw new InvalidArgumentException('You must provide a myUser object');
+    }
+
+    if (! $associationId = $this->getOption('associationId'))
+    {
+      $associationId = $user->getAssociationId();
+    }
+
+    if ($this->getObject()->isNew())
+    {
+      $this->widgetSchema['created_by'] = new sfWidgetFormInputHidden();
+      $this->widgetSchema['association_id'] = new sfWidgetFormInputHidden();
+      $this->setDefault('created_by', $user->getUserId());
+      $this->setDefault('association_id', $associationId);
+      $this->validatorSchema['association_id'] = new sfValidatorInteger();
+      $this->validatorSchema['created_by'] = new sfValidatorInteger();
+    }
+
+    $this->widgetSchema['updated_by'] = new sfWidgetFormInputHidden();
+    $this->setDefault('updated_by', $user->getUserId());
+    $this->validatorSchema['updated_by'] = new sfValidatorInteger();
+
+    $this->setLabels();
+  }
+
+  /**
+   * Set labels of the form's widgets
+   */
+  public function setLabels()
+  {
+    $this->widgetSchema->setLabels(array(
+      'date'    => 'Date',
+      'label'   => 'Libellé'
+    ));
+  }
 }
