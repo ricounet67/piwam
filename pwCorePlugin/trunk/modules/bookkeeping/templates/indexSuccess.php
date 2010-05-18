e61b3ed1c038ed7bfc2930dabaa0dac416a961d4
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-18 18:50:35 +0000

    [pwCorePlugin / trunk] Default template bookkeeping/index

--- pwCorePlugin/trunk/modules/bookkeeping/templates/indexSuccess.php
+++ pwCorePlugin/trunk/modules/bookkeeping/templates/indexSuccess.php
@@ -6,10 +6,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 ?>
+<h2>Comptabilité</h2>
+<h3>Évolutions</h3>
+<h3>Actions</h3>
+<ul>
+  <li><?php echo link_to('Nouvelle entrée', '@bk_new_entry') ?></li>
+</ul>
