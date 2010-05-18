aadb1e16e6d03feec1bc5616ee3c6081a9f3d45a
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-18 05:39:48 +0000

    [pwCorePlugin / trunk] Added default empty template indexSuccess in bookkeeping module

---
+++ pwCorePlugin/trunk/modules/bookkeeping/templates/indexSuccess.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the piwam package.
+ * (c) Adrien Mogenet <adrien.mogenet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
