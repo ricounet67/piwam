4551eaf0716525ba6de33c8d01d550ca981437da
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-18 05:32:09 +0000

    [pwCorePlugin / trunk] Added bookkeepingActions extending BaseBookkeepingActions

---
+++ pwCorePlugin/trunk/modules/bookkeeping/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../lib/BaseBookkeepingActions.class.php');
+
+/**
+ * Bookkeeping actions.
+ *
+ * @package    piwam
+ * @subpackage account
+ * @author     Adrien Mogenet
+ * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class bookkeepingActions extends BaseBookkeepingActions
+{
+}
