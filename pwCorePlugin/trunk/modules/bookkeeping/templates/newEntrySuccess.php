212d84ca64219c4230101f692accfff6c9e06bc1
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-23 09:16:23 +0000

    [pwCorePlugin / trunk] Commented JS code into bookkeeping/newEntrySuccess

--- pwCorePlugin/trunk/modules/bookkeeping/templates/newEntrySuccess.php
+++ pwCorePlugin/trunk/modules/bookkeeping/templates/newEntrySuccess.php
@@ -29,40 +29,44 @@
 <?php $counter = 0; ?>
 
 <script type="text/javascript">
+  var numberOfCredits = <?php echo $counter ?>;
 
-var cnt = <?php echo $counter ?>;
+  /**
+   * Add a new Credit form. The new credit will be numbered `num`
+   *
+   * @param   integer   num
+   * @return  string    HTML content for displaying new Credit Form
+   */
+  function addCredit(num)
+  {
+    var response = jQuery.ajax({
+      type:   'GET',
+      url:    '<?php echo url_for('bookkeeping/addCreditForm')?>' + '<?php echo ($form->getObject()->isNew()? '' : '?id=' . $form->getObject()->getId()) . ($form->getObject()->isNew() ? '?num=' : '&num=') ?>' + num,
+      async:  false
+    }).responseText;
 
-/*
- * Add a new Credit form
- */
-function addCredit(num)
-{
-  var r = jQuery.ajax({
-    type:   'GET',
-    url:    '<?php echo url_for('bookkeeping/addCreditForm')?>' + '<?php echo ($form->getObject()->isNew()? '' : '?id=' . $form->getObject()->getId()) . ($form->getObject()->isNew() ? '?num=' : '&num=') ?>' + num,
-    async:  false
-  }).responseText;
-  
-  return r;
-};
+    return response;
+  };
 
-/*
- * Delete a Credit form
- */
-function delCredit(num)
-{
-  document.getElementById('credit_container').removeChild(document.getElementById('credit_' + num));
-  cnt = cnt - 1;
-};
+  /**
+   * Delete a Credit form
+   *
+   * @param   integer   num : The number of creditForm to delete
+   */
+  function deleteCredit(num)
+  {
+    document.getElementById('credit_container').removeChild(document.getElementById('credit_' + num));
+    numberOfCredits = numberOfCredits - 1;
+  };
 
-/*
- * Add behaviour on 'add' button
- */
-jQuery().ready(function()
-{
-  jQuery('button#add_credit').click(function() {
-    jQuery("#credit_container").append(addCredit(cnt));
-    cnt = cnt + 1;
+  /*
+   * Add behaviour on the 'Add' buttons
+   */
+  jQuery().ready(function()
+  {
+    jQuery('button#add_credit').click(function() {
+      jQuery("#credit_container").append(addCredit(numberOfCredits));
+      numberOfCredits = numberOfCredits + 1;
+    });
   });
-});
 </script>
