ccceec65ed49acfa7f1cf3d4bf6f871bcadcb20c
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-24 10:37:03 +0000

    [pwCorePlugin / trunk] Updated displayed memory requirement in _isMemoryLimitHighEnough partial

--- pwCorePlugin/trunk/modules/install/templates/_isMemoryLimitHighEnough.php
+++ pwCorePlugin/trunk/modules/install/templates/_isMemoryLimitHighEnough.php
@@ -9,16 +9,16 @@
 <?php if ($error): ?>
 
     <strong>
-        <span style="font-family: courier">memory_limit</span> &lt;= 128M
+        <span style="font-family: courier">memory_limit</span> &lt;= 32M
     </strong><br />
 
     Si le paramètre <span style="font-family: courier">memory_limit</span> est
-    inférieur à 128M, Piwam pourra fonctionner mais le fonctionnement pourra
+    inférieur à 32M, Piwam pourra fonctionner mais le fonctionnement pourra
     être différent.
 
 <?php else: ?>
 
     Le paramètre <span style="font-family: courier">memory_limit</span> est
-    supérieur à 128M
+    supérieur à 32M
 
 <?php endif; ?>
