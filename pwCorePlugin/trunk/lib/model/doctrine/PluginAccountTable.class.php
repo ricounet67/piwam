19c39e80cc599fed5ea5c0fc3af15f2e5bad04a5
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-24 11:06:23 +0000

    [pwCorePlugin / trunk] Implemented AccountTable::add()

--- pwCorePlugin/trunk/lib/model/doctrine/PluginAccountTable.class.php
+++ pwCorePlugin/trunk/lib/model/doctrine/PluginAccountTable.class.php
@@ -9,5 +9,38 @@
  */
 abstract class PluginAccountTable extends Doctrine_Table
 {
+  /**
+   * Value of state field when disabled
+   *
+   * @var integer
+   */
+  const STATE_DISABLED    = 0;
 
+  /**
+   * Value of state field when enabled
+   *
+   * @var integer
+   */
+  const STATE_ENABLED     = 1;
+
+  /**
+   * Insert a new account
+   *
+   * @param   string    $label
+   * @param   string    $reference
+   * @param   integer   $associationId
+   * @return  Account   The resulting Account object
+   */
+  public static function add($code, $parent, $label, $associationId)
+  {
+    $account = new Account();
+    $account->setId($code);
+    $account->setParentId($parent);
+    $account->setAssociationId($associationId);
+    $account->setLabel($label);
+    $account->setState(AccountTable::STATE_ENABLED);
+    $account->save();
+
+    return $account;
+  }
 }
