5bdae3c481a9b1543c70e7b2d4851e70fabb3163
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-30 13:49:58 +0000

    [pwCorePlugin / trunk] Implemented AccountForm::setup()

--- pwCorePlugin/trunk/lib/form/doctrine/PluginAccountForm.class.php
+++ pwCorePlugin/trunk/lib/form/doctrine/PluginAccountForm.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Account form.
  *
@@ -10,5 +9,47 @@
  */
 abstract class PluginAccountForm extends BaseAccountForm
 {
+  /**
+   * Configure form widgets. Only the 'label' widget is displayed in any ways.
+   */
+  public function setup()
+  {
+    parent::setup();
+    $this->useFields(array('label'));
 
+
+    if (! $user = $this->getOption('user'))
+    {
+      throw new InvalidArgumentException('You must provide a myUser object');
+    }
+
+    if (! $associationId = $this->getOption('associationId'))
+    {
+      $associationId = $user->getAssociationId();
+    }
+
+    if (! $parentId = $this->getOption('parentId'))
+    {
+      throw new InvalidArgumentException('You must provide a parent account ID');
+    }
+
+    if ($this->getObject()->isNew())
+    {
+      $this->widgetSchema['created_by'] = new sfWidgetFormInputHidden();
+      $this->widgetSchema['association_id'] = new sfWidgetFormInputHidden();
+      $this->setDefault('created_by', $user->getUserId());
+      $this->setDefault('association_id', $associationId);
+      $this->validatorSchema['association_id'] = new sfValidatorInteger();
+      $this->validatorSchema['created_by'] = new sfValidatorInteger();
+      $this->widgetSchema['parent_id'] = new sfWidgetFormInputHidden();
+      $this->validatorSchema['parent_id'] = new sfValidatorInteger();
+    }
+
+    $this->setDefault('parent_id', $parentId);
+    $this->widgetSchema['label']->setAttribute('class', 'formInputLarge');
+
+    $this->widgetSchema->setLabels(array(
+      'label'     => 'Libellé',
+    ));
+  }
 }
