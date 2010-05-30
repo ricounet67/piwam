7e0d5b4a8951260aed69bfd829c96a6f1eab1b12
adrien.mogenet <adrien.mogenet@70dfc3ba-f61f-11dd-a295-0b7b0935bf94>
2010-05-30 15:58:06 +0000

    [pwCorePlugin / trunk] Fixed inheritance in accountActions

--- pwCorePlugin/trunk/modules/account/actions/actions.class.php
+++ pwCorePlugin/trunk/modules/account/actions/actions.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__).'/../lib/BaseaccountActions.class.php');
+require_once(dirname(__FILE__).'/../lib/BaseAccountActions.class.php');
 
 /**
  * Account actions.
@@ -10,6 +10,6 @@ require_once(dirname(__FILE__).'/../lib/BaseaccountActions.class.php');
  * @author     Adrien Mogenet
  * @version    SVN: $Id: actions.class.php 12474 2008-10-31 10:41:27Z fabien $
  */
-class accountActions extends BaseaccountActions
+class accountActions extends BaseAccountActions
 {
 }
